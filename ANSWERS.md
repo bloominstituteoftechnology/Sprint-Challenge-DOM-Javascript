084fe0f79eef62feb367f81577e866781115084f
Ting Wang <felix.wang.1026@gmail.com>
2017-12-01 15:21:27 -0500

    Completed the sprint requirements

---
+++ ANSWERS.md
@@ -0,0 +1,9 @@
+1. What is DOM?
+Answer: DOM is document Object Model which represents the HTML document that can be modified with a scripting language such as JavaScript. 
+DOM is an object-oriented representation of the web page. The web page is a document. the document can be displayed in the browser or as the HTML source code. So DOM represents the document with tree structure which have nodes and objects.
+
+2. What is an event?
+Anser: An event represents everything of user interactions and automated things happening during the page rendering. DOM event has additional fields and functions that can be used to let the programs do additional things to the page.
+
+3. What is an event listener?
+Answer: An event listener is an DOM event method that is added to the target event to listen for the specified event type on the DOM event on which it is called. When user interacts with the page, the event listener will capture that action to trigger the functions inside the event listener finishing doing additional things to the page.
--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,5 +1,59 @@
 class Carousel {
+  constructor(element) {
+    this.element = element;
 
+    // Get all the carousel items
+    this.items = element.querySelectorAll(".Carousel__item");
+    // set the current active carousel item to the first one
+    this.activeItem = this.items[0];
+    // set the current to zero
+    this.activeIndex = 0;
+
+    // get the left arrow and right arrow
+    this.left = element.querySelector(".Carousel__arrow-left");
+    this.right = element.querySelector(".Carousel__arrow-right");
+
+    // add event listener to left arrow and right arrow
+    this.left.addEventListener("click", (event) => {
+      this.updateActive("left");
+    });
+    this.right.addEventListener("click", (event) => {
+      this.updateActive("right");
+    });
+  }
+
+  updateActive(arrow) {
+    if (arrow === null) return;
+    
+    // deselect current item
+    this.deselect();
+
+    if (arrow === "left") {
+      // if current item is the first one, when clicking left arrow, display the last item
+      if (this.activeIndex === 0) {
+        this.activeIndex = this.items.length - 1;
+      } else {
+        this.activeIndex--;
+      }
+    } else if (arrow === "right") {
+      // if current item is the last one, when clicking right arrow, display the first item
+      if (this.activeIndex === this.items.length - 1) {
+        this.activeIndex = 0;
+      } else {
+        this.activeIndex++;
+      }
+    }
+    this.activeItem = this.items[this.activeIndex];
+    this.select();
+  }
+
+  select() {
+    this.activeItem.classList.add("Carousel__item-focused");
+  }
+
+  deselect() {
+    this.activeItem.classList.remove("Carousel__item-focused");
+  }
 }
 
 let carousels = document.querySelectorAll(".Carousel");

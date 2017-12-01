0edf4c218b9d5637f6e0d7d66251837333acae89
dreamingrainbow <support@dreamingrainbow.com>
2017-12-01 20:11:01 +0000

    Was.. helping others hahahaha

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,5 +1,26 @@
 class Carousel {
-
+    constructor(carousel) {
+        this.carousel = carousel;
+        this.itemList = Array.from(carousel.querySelectorAll('.Carousel__item'));
+        this.current = this.itemList.shift();
+    }
+    
+    prev() {
+        this.current.classList.remove('Carousel__item-focused');
+        const temp = this.current;
+        this.current = this.itemList.shift();
+        this.current.classList.add('Carousel__item-focused');
+        this.itemList.push(temp);
+    }
+    
+    next() {
+        this.current.classList.remove('Carousel__item-focused');
+        const temp = this.current;
+        this.current = this.itemList.pop();
+        this.current.classList.add('Carousel__item-focused');
+        this.itemList.unshift(temp);
+        
+    }
 }
 
 let carousels = document.querySelectorAll(".Carousel");

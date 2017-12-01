15e92bfa470350812f8dc1e59cc0b82d6deafcf1
JacquelynnRohrich <33401573+JacquelynnRohrich@users.noreply.github.com>
2017-12-01 13:55:44 -0600

    answers finished no js

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,6 +1,34 @@
 class Carousel {
+constructor(element) {
+    this.element= element;
+}
+
+select() {
+    this.element.classList.add('Carousel__item');
+    this.Carousel.select();
+}
+deselect (){
+    this.element.classList.remove('Carousel__item')
+    this.Carousel.deselect();
+}
+// I assumed i needed to make classes for these functions to work but I could be far off subject of what you were looking for. 
+class Carousel__arrow-left {
 
 }
 
+class Carousel__arrow-right{
+    
+
+class Carousel__items {
+
+}
+
+class Carousel__item {    
+
+}
+
+
 let carousels = document.querySelectorAll(".Carousel");
 carousels = Array.from(carousels).map(carousel => new Carousel(carousel));
+
+//I need to understand more what is needed from me on this. I barely grasped on the tabs creation and this is just to advanced for me right meow. 

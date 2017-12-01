30a0dce72cd6a3be0ad1a9e277d086274558e2d9
Jamie Domingo <jaybmw@aol.com>
2017-12-01 11:18:17 -0800

    Complete

---
+++ ANSWERS.md
@@ -0,0 +1,14 @@
+
+
+What is the DOM?
+
+	DOM is the way javascript can be used to manipulate html elements in order to create dynammic web pages. 
+
+
+What is an event?
+
+	An event is something that the user does on the website interface that connects to an html element.
+
+What is an event listener?
+
+	An event listener checks if an action was done by the user in order to execute a method. 
--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,6 +1,51 @@
 class Carousel {
+	constructor(element) {
+		this.element = element;
+		this.arrows = this.element.querySelectorAll(".Carousel__arrow");
+		this.counter = 0;
+		this.box = new Box(this.element);
+		Array.from(this.arrows).forEach((arrow) => {
+			arrow.addEventListener('click', () => {
+				let previousCounter = this.counter;
+				if (arrow.classList.contains("Carousel__arrow-left")) {
+					console.log('left arrow clicked');
+					this.counter--;
+				}
+				if (arrow.classList.contains("Carousel__arrow-right")) {
+					console.log('right arrow clicked');
+					this.counter++;
+				};
+				if (this.counter === -1) {
+				this.counter = 2;
+				}
+				if (this.counter === 3) {
+				this.counter = 0;
+				}
+				this.box.select(this.counter);
+				this.box.deselect(previousCounter);
+			});
+		});
+	}
+}
+
+class Box {
+	constructor(element) {
+		this.element = element;
+		this.box = element.querySelectorAll(".Carousel__item");
+	}
+
+	select(counter) {
+		Array.from(this.box)[counter].classList.add("Carousel__item-focused");
+	}
+
+	deselect(previousCounter) {
+		Array.from(this.box)[previousCounter].classList.remove("Carousel__item-focused");
+	}
+
 
 }
+
+
 
 let carousels = document.querySelectorAll(".Carousel");
 carousels = Array.from(carousels).map(carousel => new Carousel(carousel));

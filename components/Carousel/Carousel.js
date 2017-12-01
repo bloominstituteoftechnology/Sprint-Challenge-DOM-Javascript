8ab02a1b803c468749061a2fa1842a963b6ba863
Kwaku Dapaah <kwakudapaah@Kwakus-MBP.fios-router.home>
2017-12-01 15:06:32 -0500

    changes

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,6 +1,28 @@
 class Carousel {
+   constructor(element){
+     this.element = element;
+     
 
+     select() {
+
+     }
+
+     unselect() {
+
+     }
+
+     
+
+   }
 }
+class Navigate { //takes in left or right direction and moves
+	constructor (element) {
+		this.element = element;
+	}
+        this.direction = element.querySelectorAll('Carousel__items');
+     }
+
+
 
 let carousels = document.querySelectorAll(".Carousel");
 carousels = Array.from(carousels).map(carousel => new Carousel(carousel));

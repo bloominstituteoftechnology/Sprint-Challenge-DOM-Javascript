01b693178461b6e6138056b2269fff1203c402a8
Yasin Shuman <email@yasin.io>
2017-12-01 14:31:32 -0800

    added moveLeft()

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -6,10 +6,26 @@ class Carousel {
     // console.log(this.itemList);
     this.active = this.itemList.shift();
     // console.log(this.active);
+    this.moveLeft();
   }
 
   moveLeft() {
-    this.active.remove('.Carousel__item-focused');
+    console.log("starting active: ");
+    console.log(this.active);
+    this.active.classList.remove("Carousel__item-focused");
+    console.log("should have removed item-focused class: ")
+    console.log(this.active); // remove the class off the active item
+    this.itemList.push(this.active); // add the item to the end of the array
+    console.log(this.itemList);
+    this.active = this.itemList.shift(); // assign the first item in array to active
+    console.log(this.active);
+    this.active.classList.add(".Carousel__item-focused"); // add the class onto active to make it visible
+    console.log("updated item list");
+    console.log(this.itemList);
+    console.log("updated active item");
+    console.log(this.active);
+    this.itemList.push(this.active);
+    console.log(this.itemList);
   }
 
   moveRight() {

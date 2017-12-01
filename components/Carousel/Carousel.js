93e29091f2d4332f9f2a61388552b0012e02a7f3
Adam Fetters <adamfetterscoder@gmail.com>
2017-12-01 13:31:47 -0500

    Adding event listeners

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,6 +1,42 @@
 class Carousel {
+  constructor(elment) {
+    this.element = element;
+    this.carouselItems = element.querySelectorAll('.Carousel__items');
+    this.carouselItems = Array.from(this.carouselItems).map((item) => {
+      return new CarouselItem(item, this);
+    });
+    this.current = this.carouselItems[0];
+    
+    let count = 0;
+    const amount = this.carouselItems.length;
+  }
 
+  navigate(direction) {
+    this.current.classList.remove('Carousel__item-focused');
+    this.counter += direction;
+    if (direction === -1 && counter < 0) {
+      counter = amount - 1;
+    }
+    if (direction === 1 && counter > amount) {
+      counter = 0;
+    }
+    this.current = items[current];
+    current.classList.add('Carousel__item-focused');
+  }
+  
 }
 
-let carousels = document.querySelectorAll(".Carousel");
+class CarouselItem {
+  constructor(element, parent) {
+    this.element = element;
+    this.carousel = parent;
+
+    this.element.addEventListener('click')
+  }
+
+  
+}
+
+let carousels = document.querySelectorAll('.Carousel');
+console.log(carousels);
 carousels = Array.from(carousels).map(carousel => new Carousel(carousel));

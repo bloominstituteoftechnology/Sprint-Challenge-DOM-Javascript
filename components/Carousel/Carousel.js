4dc342674929bfcb2aded43388afce708590e461
Mark Marchant <jtla3@yahoo.com>
2017-12-01 15:05:13 -0800

    Started the Carousel Class but not finished

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,5 +1,26 @@
 class Carousel {
+    constructor(element) {
+       // attaches dom element to an object 
+       this.element = element
+        // gets all items from Carousel
+        this.items = element.querySelectorAll(".Carousel__item");
+        // set the current Carousel item to the first item
+        this.activeItem = this.items[0];
+        // set the current to zero
+        this.activeIndex = 0;
 
+        // get the left and right Carousel arrows
+        this.left = element.querySelector(".Carousel__arrow-left");
+        this.right = element.querySelector(".Carousel__arrow-right");
+
+        // add event listener to left and right Carousel arrows
+        this.left.addEventListener("click", (event) => {
+            this.updateActive("left");
+        });
+        this.right.addEventListener("click", (event) => {
+            this.updateActive("right");
+        });
+    }
 }
 
 let carousels = document.querySelectorAll(".Carousel");

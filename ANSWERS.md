ad99f11babda959266b5aa674f6854d3d46e3362
cmoore4os <cm4glos@gmail.com>
2017-12-01 15:26:06 -0500

    Consuella Moore -- initial commit

---
+++ ANSWERS.md
@@ -0,0 +1,9 @@
+# Answers
+
+1. What is the Dom? In html it is the browser window tree of elements.
+
+2. What is an event? A script that will happen when some other action is done.
+
+3. What is an event listener? Is an action waiting for a response before doing
+   something else. Example a button waiting to be clicked on before going to a
+   new page
--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,6 +1,51 @@
-class Carousel {
+class CarouselItem {
+  // turn each carousel item into an object with a focus and unfocus methods
+  constructor(element) {
+    this.element = element;
+  }
+  focus() {
+    this.element.classList.add('Carousel__item--focused');
+  }
 
+  unfocus() {
+    this.element.classList.remove('Carousel__item--focused');
+  }
+}
+class Carousel {
+  constructor(element) {
+    // get a carousel
+    this.element = element;
+    // place all carousel items in an arr/objects
+    this.items = element.querySelectorAll('.Carousel__item');
+    console.log(this.items);
+    // get left and right arrows
+    this.focusedItem = element.getElementsByClassName(
+      'Carousel__item--focused'
+    );
+    this.leftArrow = element.getElementsByClassName('Carousel__arrow--left');
+    this.rightArrow = element.getElementsByClassName('Carousel__arrow--right');
+
+    // listen for left arrow click
+    this.leftArrow.addEventListener('click', event => {
+      // if (focusedItem !== this.items[0]) {
+      // add .Carousel__item--focused to this.items[i - 1],
+      // remove .Carousel__item--focused to this.items[i]
+      // focusedItem = this.item
+      if (0 === 0) {
+        console.log('ToDo');
+      }
+    });
+    // listen for right arrow click
+    this.rightArrow.addEventListener('click', event => {
+      // if (focusedItem !== this.items.length - 1) {
+      // add .Carousel__item--focused to this.items[i + 1],
+      // remove .Carousel__item--focused to this.items[i]
+      if (0 === 0) {
+        console.log('ToDo');
+      }
+    });
+  }
 }
 
-let carousels = document.querySelectorAll(".Carousel");
+let carousels = document.querySelectorAll('.Carousel');
 carousels = Array.from(carousels).map(carousel => new Carousel(carousel));

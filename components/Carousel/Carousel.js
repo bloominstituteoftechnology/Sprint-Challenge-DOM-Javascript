32399dc7a97ea31653d19fd6a4f1cb299d1738b8
Xiaoping Li <narci2008@gmail.com>
2017-12-01 15:43:55 -0800

    complete challenge

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,24 +1,44 @@
-class Carousel {
+// class Carousel {
 
-}
-
-let carousels = document.querySelectorAll(".Carousel");
-carousels = Array.from(carousels).map(carousel => new Carousel(carousel));
-
-// /* Simple JS Functions */
-// const slideArr = [1, 2, 3];
-// const leftSlide = (arr) => {
-//   let slide;
-//   for (let i = arr.length - 1; i >= 0; i--) {
-//      slide = arr[i];
-//      console.log(slide);
-//   }
 // }
 
-// const rightSlide = (arr) => {
-//   let slide;
-//   for (let i = 0; i < arr.length; i++) {
-//      slide = arr[i];
-//      console.log(slide);
-//   }
-// }
+// let carousels = document.querySelectorAll(".Carousel");
+// carousels = Array.from(carousels).map(carousel => new Carousel(carousel));
+
+/* JS Functions without Class */
+const slide = document.querySelectorAll(".Carousel__item");
+const slideArr = Array.from(slide);
+let i = 0
+let activeSlide = slideArr[i];
+
+const leftClick = () => {
+  i = i - 1;
+  activeSlide = slideArr[i];
+  if (i - 1 < -1) {
+    i = slideArr.length - 1;
+    activeSlide = slideArr[i];
+  }
+  return activeSlide;
+};
+
+const rightClick = () => {
+  i = i + 1;
+  activeSlide = slideArr[i];
+  if (i + 1 > slideArr.length) {
+    i = 0;
+    activeSlide = slideArr[i];
+  }
+  return activeSlide;
+};
+
+document.querySelector(".Carousel__arrow-left").addEventListener("click", function() {
+			activeSlide.classList.remove("Carousel__item-focused");
+			leftClick();
+			activeSlide.classList.add("Carousel__item-focused");
+		});
+
+document.querySelector(".Carousel__arrow-right").addEventListener("click", function() {
+			activeSlide.classList.remove("Carousel__item-focused");
+			rightClick();
+			activeSlide.classList.add("Carousel__item-focused");
+		});

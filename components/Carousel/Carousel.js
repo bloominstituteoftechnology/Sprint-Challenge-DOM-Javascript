e2f5b9557559dd0d4fa9081174204917a656544d
Patrick Sandoval <patsand24csu@yahoo.com>
2017-12-01 12:04:20 -0800

    No solution, need to fix functionality and add helper functions

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,8 +1,14 @@
 class Carousel {
   constructor(element) {
     this.element = element;
+    this.index = 0;
+    this.movingRight = element.querySelector('.Carousel__arrow-right');
+    this.movingLeft = element.querySelector('.Carousel__arrow-left');
+    this.carouselItems = Array.from(this.element.querySelectorAll(".Carousel__item"));
   }
 
+  // select and deselect might not be necessary in a refactor
+  // since I'm tryinf to implement in move functions.
   select() {
     this.element.classList.add('Carousel__item-focused');
   }
@@ -10,12 +16,32 @@ class Carousel {
   deselect() {
     this.element.classList.remove('Carousel__item-focused');
   }
-}
 
-class CarouselMovement {
-  constructor(element) {
-    this.element = element;
-    
+  moveRight() {
+    if (this.element !== this.element.length - 1) {
+      this.carouselItems[this.index].classList.remove('Carousel__item-focused');
+      // ++ because we are moving right on index
+      this.carouselItems[++this.index].classList.add('Carousel__item-focused');
+  }
+
+  //event of moving, maybe in its own function later?
+  this.movingRight.addEventListener('click', () => {
+    this.moveRight();
+  });
+
+  moveLeft() {
+    if (this.index !== 0) {
+      this.carouselItems[this.index].classList.remove('Carousel__item-focused');
+      // -- because we are moveing left on index
+      this.carouselItems[--this.index].classList.add('Carousel__item-focused');
+    }
+  }
+  
+  // Again should really be in its own function I think
+  this.movingLeft.addEventListener('click', () => {
+    this.moveLeft();
+  });
+
   }
 }
 

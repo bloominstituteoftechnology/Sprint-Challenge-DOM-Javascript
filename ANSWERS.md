aa653c366eb1172e382a9ad4822191881d96fea7
AustenHowes <austie702@gmail.com>
2017-12-01 14:05:50 -0600

    IOU

---
+++ ANSWERS.md
@@ -0,0 +1,9 @@
+## Questions
+1. What is the DOM?
+---Document Object Model. This is the tree structure containing all the objects and elements in a website. 
+
+2. What is an event?
+---Events run a wide gamut of examples, but the simplest form is a mouse click. If mouse clicks (or hovers, or unclicks, etc) then run this function of code. Events can be many things.
+
+2. What is an event listener?
+---Simply the code that waits for an event rule to be fulfilled and detected in order to execude the desired code.
--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,6 +1,76 @@
 class Carousel {
-
-}
+    constructor(element) {
+      this.element = element;
+    }
+  
+    select() {
+      this.element.classList.add("Carousel__item-focused");
+    }
+  
+    deselect() {
+      this.element.classList.remove("Carousel__item-focused");
+    }
+  }
+  
+  class TabLink {
+    constructor(element) {
+      this.element = element;
+      this.element.addEventListener('click', (event) => {
+        event.tabData = this.element.dataset.tab;
+      });
+    }
+  
+    select() {
+      this.element.classList.add("Tabs__link--selected");
+    }
+  
+    deselect() {
+      this.element.classList.remove("Tabs__link--selected");
+    }
+  }
+  
+  class Tabs {
+    constructor(element) {
+      this.element = element;
+  
+      // Attaches links with data-tab value as property
+      this.links = element.querySelectorAll(".Tabs__link");
+      this.links = Array.from(this.links).reduce((obj, link) => {
+        obj[link.dataset.tab] = new TabLink(link);
+        return obj;
+      }, {});
+  
+      // Attaches items with data-tab value as property
+      this.items = element.querySelectorAll(".Tabs__item");
+      this.items = Array.from(this.items).reduce((obj, item) => {
+        obj[item.dataset.tab] = new TabItem(item);
+        return obj;
+      }, {});
+  
+      // Listens for a click event in its children or self
+      this.element.addEventListener('click', (event) => {
+        if (event.tabData) {
+         this.updateActive(event.tabData);
+         event.stopPropagation(); 
+        }
+      })
+  
+      this.activeData = element.querySelector(".Tabs__default");
+      this.activeData = this.activeData ? this.activeData.dataset.tab : null;
+      this.updateActive(this.activeData);
+    }
+  
+    updateActive(data) {
+      if (data === null) return;
+      this.links[this.activeData].deselect();
+      this.items[this.activeData].deselect();
+  
+      this.links[data].select();
+      this.items[data].select();
+      this.activeData = data;
+    }
+  
+  }
 
 let carousels = document.querySelectorAll(".Carousel");
 carousels = Array.from(carousels).map(carousel => new Carousel(carousel));

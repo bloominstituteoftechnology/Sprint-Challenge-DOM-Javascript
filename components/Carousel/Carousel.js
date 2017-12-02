a9dab84395fd1b2bc63de7ed84b61dad77f820da
yasin shuman <yshuman@loyalloops.com>
2017-12-02 01:46:22 +0000

    complete!

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,35 +1,22 @@
 class Carousel {
   constructor(element) {
-    // console.log(element);
     this.element = element;
     this.itemList = Array.from(this.element.querySelectorAll(".Carousel__item"));
-    // console.log(this.itemList);
     this.active = this.itemList.shift();
-    // console.log(this.active);
-    this.moveLeft();
   }
 
   moveLeft() {
-    console.log("starting active: ");
-    console.log(this.active);
-    this.active.classList.remove("Carousel__item-focused");
-    console.log("should have removed item-focused class: ")
-    console.log(this.active); // remove the class off the active item
+    this.active.classList.remove("Carousel__item-focused"); // remove the class off the active item
     this.itemList.push(this.active); // add the item to the end of the array
-    console.log(this.itemList);
     this.active = this.itemList.shift(); // assign the first item in array to active
-    console.log(this.active);
-    this.active.classList.add(".Carousel__item-focused"); // add the class onto active to make it visible
-    console.log("updated item list");
-    console.log(this.itemList);
-    console.log("updated active item");
-    console.log(this.active);
-    this.itemList.push(this.active);
-    console.log(this.itemList);
+    this.active.classList.add("Carousel__item-focused"); // add the class onto active to make it visible
   }
 
   moveRight() {
-
+    this.active.classList.remove("Carousel__item-focused"); // remove the class off the active item
+    this.itemList.unshift(this.active); // will load this.active onto front of array
+    this.active = this.itemList.pop(); // assign the last element to active
+    this.active.classList.add("Carousel__item-focused"); // add the class onto active to make it visible
   }
 
 }

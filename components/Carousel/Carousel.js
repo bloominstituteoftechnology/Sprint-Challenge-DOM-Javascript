4f1c3594cb605405cbb16d0171ac34c8d0d5ddde
Camila Daniels <camila.daniels926@gmail.com>
2017-12-22 14:01:33 -0600

    Camila Daniels camila.daniels926@gmail.com

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,6 +1,45 @@
 class Carousel {
+	constructor(element) {
+		this.element = element;
+		this.leftArrows = element.querySelectorAll(".Carousel__arrow-left");
+		this.leftArrows = Array.from(this.leftArrows).map((arrow) => {
+			return new CarouselArrowLeft(arrow, this);
+		});
+		this.rightArrows = element.querySelectorAll(".Carousel__arrow-right"); 
+		this.rightArrows = Array.from(this.rightArrows).map((arrow) => {
+			return new CarouselArrowRight(arrow, this);
+		})
+		this.activeItem = this.rightArrows[0];
+		this.init();
+	}
+
+	init() {
+		this.rightArrows[0].select();
+	}
+
+	updateActive(newActive) {
+		this.activeItem.deselect();
+		this.activeItem = newActive;
+	}
+
+	getTab(data) {
+		return this.element.querySelector('Carousel__item-focused');
+	}
 
 }
+
+class CarouselItem {
+
+}
+
+class CarouselArrowLeft {
+
+}
+
+class CarouselArrowRight {
+
+}
+
 
 let carousels = document.querySelectorAll(".Carousel");
 carousels = Array.from(carousels).map(carousel => new Carousel(carousel));

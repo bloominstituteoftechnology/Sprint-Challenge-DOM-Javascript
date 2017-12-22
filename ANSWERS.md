1e67a84047c01ebb2ba7bf1a3b47203bc7d858fb
theMultitude <os.schroeder@gmail.com>
2017-12-22 13:56:22 -0500

    C Schroeder DOM Sprint

---
+++ ANSWERS.md
@@ -0,0 +1,8 @@
+1. What is the DOM?
+    * DOM stands for Document Object Manipulation and is an interface that uses Javascript to coordinate html and css interaction.  It represents html in a tree structure of objects.
+
+2. What is an event?
+    * An event is some occurance or process initiated at some point in time in code.  It could be a click or an occurance on a timer but it is a discrete occurance (or at least has a discrete beginning).
+    
+3. What is an event listener?
+    * An event listener is a piece of code that watches for certain predefined events and executes a process or processes as as a reaction.
--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,6 +1,68 @@
 class Carousel {
+    constructor (element) {
+        this.element = element
+
+        // collects carousel items
+        this.items = element.querySelectorAll('Carousel__item')
+        this.items = Array.from(this.items)
+
+        // set default position
+        this.element.classList.add('Carousel__item-focused')
+        this.current = this.items[0]
+
+        let right = element.querySelector("Carousel__arrow-right")
+        let left = element.querySelector("Carousel__arrow-right")
+        // create a counter to compare with Array.length (++ upon changeRight __ upon changeLeft)
+        let counter = 0
+        }
+        
+        // add eventlistener (click) --> "Carousel arrow right" -- changeRight        
+        this.element.addEventListener('click', (event) => {
+            if(event.right) {
+                changeRight();
+            } // Ran out of time but I may have realized why the links class was created in the Tabs work you did
+              // Also seems I haven't quite worked out the kinks using listeners
+        } )
+
+        // add eventlistener (click) --> "Carousel arrow left" -- changeLeft
+        this.element.addEventListener('click', (event) => {
+            if(event.left) {
+                changeLeft()
+            }
+    }
+}
+
+    changeRight() {
+        counter++;
+        this.element.classList.remove(this.current)
+        if(counter > this.items.length) {
+            this.current = this.items[0];
+            counter = 0
+        } else {
+            this.current = this.items[counter - 1]
+        }
+        this.element.classList.add(this.current)
+        // upon click change focus from this.items.current --> this.items.next (next item in the array)
+        // if counter > array.length --> change to array[0]
+
+    }
+
+
+    changeLeft() {
+        counter--;
+        this.element.classList.remove(this.current)
+        if(counter < 0) {
+            this.current = this.items[this.items.length - 1];
+            counter = this.items.length - 1
+        } else {
+            this.current = this.items[counter - 1]
+        }
+        this.element.classList.add(this.current)
+        // upon click change focus from this.items.current --> this.items.next (next item in the array)
+        // if counter > array.length --> change to array[array.length - 1]
 
 }
+
 
 let carousels = document.querySelectorAll(".Carousel");
 carousels = Array.from(carousels).map(carousel => new Carousel(carousel));

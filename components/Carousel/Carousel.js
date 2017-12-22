53c6a5cb652648aab5bb4fdd8f8c7436b3ab8459
Clara Andrew <claraandrew@Claras-MacBook-Pro.local>
2017-12-22 16:03:15 -0500

    cleaned up code

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,3 +1,68 @@
+class Carousel {
+    constructor(element) {
+        this.element = element;
+        this.items = document.querySelectorAll(".Carousel__item");
+        this.items = Array.from(this.items)
+    
+        this.indexPosition = 0;
+    
+        this.leftArrow = document.querySelector(".Carousel__arrow-left");
+        this.rightArrow = document.querySelector(".Carousel__arrow-right");
+
+        this.leftArrow.addEventListener('click', (event) => {
+            this.indexPosition -= 1;
+            this.updateActive();
+        });
+
+        this.rightArrow.addEventListener('click', (event) => {
+            this.indexPosition += 1;
+            this.updateActive();
+        });
+    
+        }
+
+    lastItemIndex() {
+        return this.items.length - 1;
+    }
+
+    updateActive() {
+        if (this.indexPosition < 0) {
+            this.indexPosition = 0;
+            this.deselect();
+            this.select();
+        }
+        if (this.indexPosition >= this.lastItemIndex()) {
+            this.indexPosition = this.lastItemIndex();
+            this.deselect();
+            this.select();
+        }
+        else {
+            this.deselect();
+            this.select();
+        }
+    }
+
+    select() {
+        this.items[this.indexPosition].classList.add("Carousel__item-focused");
+    }
+
+    deselect() {
+        this.items.map(item => item.classList.remove("Carousel__item-focused"));
+    }
+
+}
+      
+let carousels = document.querySelectorAll(".Carousel");
+carousels = Array.from(carousels).map(carousel => new Carousel(carousel));
+
+
+
+// need to create an array and then filter by position;
+    // in an array [1,2,3] if right is clicked array would provide the item from index position +1
+        // if no position exist then stay on current
+    // in an array [1,2,3] if left is clicked array would provide the previous index (index-1)
+        // if previous index doesn't exist then stay on current
+
 // None class method has carousel working
 // let indexPosition = 0;
 // let items = document.querySelectorAll(".Carousel__item");
@@ -43,79 +108,3 @@
 //         arrayItems[indexPosition].classList.add("Carousel__item-focused");
 //     }
 // }); 
-
-class Carousel {
-    constructor(element) {
-        this.element = element;
-        console.log(this.element);
-        this.items = document.querySelectorAll(".Carousel__item");
-        this.items = Array.from(this.items)
-        console.log(this.items)
-    
-        this.indexPosition = 0;
-    
-        this.leftArrow = document.querySelector(".Carousel__arrow-left");
-        this.rightArrow = document.querySelector(".Carousel__arrow-right");
-        console.log(document.querySelector(".Carousel__arrow-right"))
-
-        this.leftArrow.addEventListener('click', (event) => {
-            this.indexPosition -= 1;
-            this.updateActive();
-            console.log("left arrow has been clicked!")
-            // event.stopPropagation();
-        });
-
-        this.rightArrow.addEventListener('click', (event) => {
-            this.indexPosition += 1;
-            this.updateActive();
-            console.log("right arrow has been clicked!")
-            event.stopPropagation();
-        });
-    
-        }
-
-    lastItemIndex() {
-        return this.items.length - 1;
-    }
-
-    updateActive() {
-        if (this.indexPosition < 0) {
-            this.indexPosition = 0;
-            this.deselect();
-            this.select();
-            console.log(this.indexPosition);
-        }
-        if (this.indexPosition >= this.lastItemIndex()) {
-            this.indexPosition = this.lastItemIndex();
-            this.deselect();
-            this.select();
-            console.log(this.indexPosition);
-        }
-        else {
-            this.deselect();
-            this.select();
-            console.log(this.indexPosition);
-        }
-    }
-
-    select() {
-        this.items[this.indexPosition].classList.add("Carousel__item-focused");
-    }
-
-    deselect() {
-        this.items.map(item => item.classList.remove("Carousel__item-focused"));
-    }
-
-}
-      
-let carousels = document.querySelectorAll(".Carousel");
-carousels = Array.from(carousels).map(carousel => new Carousel(carousel));
-
-
-
-// need to create an array and then filter by position;
-    // in an array [1,2,3] if right is clicked array would provide the item from index position +1
-        // if no position exist then stay on current
-    // in an array [1,2,3] if left is clicked array would provide the previous index (index-1)
-        // if previous index doesn't exist then stay on current
-

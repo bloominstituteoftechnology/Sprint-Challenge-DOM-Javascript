23c4b2da7ff6fc106dfca7a944d4ca8fdccf1265
Troy Williams <troywilliams@Troys-MacBook-Pro.local>
2017-12-22 12:43:45 -0600

    class not implemented but carousel is working

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,5 +1,41 @@
 class Carousel {
+  // constructor(){
+  //   this.activeItem = document.querySelector('.Carousel__item-focused');
+  //   this.leftArrow = document.querySelector('.Carousel__arrow-left');
+  //   this.rightArrow = document.querySelector('.Carousel__arrow-right');
+  //   this.carouselItems = document.querySelectorAll('.Carousel__item');
+  //   this.carouselItems = Array.from(carouselItems);
+  // }
 
+  // moveRight() {
+  //   if (this.carouselItems.indexOf(this.activeItem) === this.carouselItems.length - 1) {
+  //     this.activeItem = this.carouselItems[0];
+  //   }
+  //   this.activeItem = this.carouselItems[this.carouselItems.indexOf(this.activeItem) + 1];
+  // }
+  // moveLeft() {
+  //   if (this.carouselItems.indexOf(this.activeItem) === 0) {
+  //     this.activeItem = this.carouselItems[this.carouselItems.length - 1];
+  //   }
+  //   this.activeItem = this.carouselItems[this.carouselItems.indexOf(this.activeItem) - 1];
+  // }
+  // deselect() {
+  //   console.log(this.activeItem);
+  //   this.activeItem.classList.remove('Carousel__item-focused');
+  // }
+  // select() {
+  //   this.activeItem.classList.add('Carousel__item-focused');
+  // }
+  // leftArrow.addEventListener('click', () => {
+  //   deselect();
+  //   moveLeft();
+  //   select();
+  // }
+  //
+  // this.rightArrow.addEventListener('click', () => {
+  //   this.activeItem.classList.remove('Carousel__item-focused');
+  //   moveRight();
+  //   this.activeItem.classList.add('Carousel__item-focused');
 }
 let carousels = document.querySelectorAll(".Carousel");
 carousels = Array.from(carousels).map(carousel => new Carousel(carousel));

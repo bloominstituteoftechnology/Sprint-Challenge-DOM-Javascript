c38bbfb21def6f5290d18ae58d92cfb6ae6fda91
unknown <brcampbell001@outlook.com>
2017-12-22 12:56:24 -0600

    Ben Campbell DOM Sprint - Answers Complete/Still thinking through challenge

---
+++ ANSWERS.md
@@ -0,0 +1,9 @@
+## Questions
+1. What is the DOM?
++ DOM stands for Document Object Model. How I understand it on a surface level is that it is the interpreter and fixer for HTML and Javascript. For HTML, the DOM can often feel in the blanks as it were to make sure the HTML is executed properly by the browser. Similarly, Javascript is simply a language that the browser can read, but the DOM us where the stuff actually happens. The DOM is emitting the behavior and it's where everything happens in the browser.
+
+2. What is an event?
++ An event is simply a behavior emitted when a certain javascript program is called when, say, a user clicks on an HTML element. Basically, it sets the stage for you to be able to respond to certain actions committed by your users.
+
+3. What is an event listener?
++ The event listener is basically a method attached to a specific element to watch for when behaviors that you want to capture from your user are emitted.
--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,4 +1,29 @@
 class Carousel {
+    constructor(element) {
+        this.element = element; //after setting the constructor, you want select all of the carousel items
+        this.items = element.querySelectorAll(".Carousel__item"); // next set the current item that is active to the first one
+        this.activeItem = this.items[0]; // finally the current active needs to be set to 0
+        this.activeIndex = 0;
+
+        /* Start Left and right arrow functionality /*
+        
+        // grab both arrows
+        this.left = element.querySelector(".Carousel__arrow-left");
+        this.right = element.querySelector(".Carousel__arrow-right");
+
+        //need to add event listeners for both arrows #updateactive
+
+
+
+        //deselect current stuff
+        this.deselect();
+
+        // if else statement to make it go back to last item of carousel if left arrow is clicked on first image
+        // and maybe else if that ment for clicking right arrow to cycle from end to beginning image (or just duplicate for else statement above but for right see which one works)
+        // add a select and deselect at end to work on focus for carousel
+
+        // taking a quick break to think through this and will try and come back to finish up
+    }
 
 }
 

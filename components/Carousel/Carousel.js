302ab66615233d3cc5925742943bfe0e62567018
Camila Daniels <camila.daniels926@gmail.com>
2017-12-22 18:08:23 -0600

    Camila Daniels camila.daniels926@gmail.com

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -22,22 +22,69 @@ class Carousel {
 		this.activeItem = newActive;
 	}
 
-	getTab(data) {
+	getCarousel(data) {
 		return this.element.querySelector('Carousel__item-focused');
 	}
-
 }
 
 class CarouselItem {
+	constructor(element) {
+		this.element = element;
+	}
 
+	select() {
+		this.element.classList.add("Carousel__item-focused");
+	}
+
+	deselect() {
+		this.element.classList.remove("Carousel__item-focused");
+	}
 }
 
 class CarouselArrowLeft {
+	constructor(element, parent) {
+		this.element = element;
+		this.carousels = parent;
+		this.carouselItem = parent.getCarousel(this.element.dataset.carousel);
+		this.carouselItem = new CarouselItem(this.carouselItem);
+		this.element.addEventListener('click', () => {
+			this.carousels.updateActive(this);
+			this.select();
+		});
+	}
 
+	select() {
+		this.element.classList.add("Carousel__item-focused");
+		this.carouselItem.select();
+	}
+
+	deselect() {
+		this.element.classList.remove("Carousel__item-focused");
+		this.carouselItem.deselect();
+	}
 }
 
 class CarouselArrowRight {
+	constructor(element, parent) {
+		this.element = element;
+		this.carousels = parent;
+		this.carouselItem = parent.getCarousel(this.element.dataset.carousel);
+		this.carouselItem = new CarouselItem(this.carouselItem);
+		this.element.addEventListener('click', () => {
+			this.carousels.updateActive(this);
+			this.select();
+		});
+	}
 
+	select() {
+		this.element.classList.add("Carousel__item-focused");
+		this.carouselItem.select();
+	}
+
+	deselect() {
+		this.element.classList.remove("Carousel__item-focused");
+		this.carouselItem.deselect();
+	}
 }
 
 

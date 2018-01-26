221777758abfceeb753a156c30de8d0b89198c48
Christopher Beards <beardscm@gmail.com>
2018-01-26 14:10:04 -0500

    1.26.2018

---
+++ ANSWERS.md
@@ -0,0 +1,13 @@
+## Questions
+1. What is the DOM?
+# The DOM is the Document Object Model. It allows us to view, modify, and interact with webpages and applications.
+# The browser document is able to receive various codes and translate it into something more appealing. We use the DOM
+# to make all of this happen. Code is what happens when you prepare a recipie, but the DOM allows us to experience
+# the finished product.
+
+2. What is an event?
+# An event is any sort of thing that takes place within the Document Object. There are various events that can be 
+# listened for, and subsequently sewn into our applications and websites for interactivity.
+
+2. What is an event listener?
+# An event listener is the way we monitor and work with events. 
--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,6 +1,39 @@
-class Carousel {
+  // let slider = document.querySelector('Carousel__item-focused')
+  let slides = document.querySelectorAll('Carousel__item');
+  let r = document.getElementById('R');
+  let l = document.getElementById('L');
+  let current = slides[0];
+  
+  function previous() {
+  slides[current - 1];
+  current--;
+  }
+  
+  function next() {
+    slides[current + 1];
+    current++;
+  }
 
-}
+// Previous
+l.addEventListener('onclick', function() {
+  console.log('clicked left');
+  if (current === 0) {
+    current = slides.length;
+  }
+  previous();
+});
 
-let carousels = document.querySelectorAll(".Carousel");
-carousels = Array.from(carousels).map(carousel => new Carousel(carousel));
+// Next
+r.addEventListener('click', function() {
+  console.log('clicked right');
+  if (current === slides.length - 1) {
+    current = -1;
+  }
+  next();
+});
+
+
+
+
+// let carousels = document.querySelectorAll(".Carousel");
+// carousels = Array.from(carousels).map(carousel => new Carousel(carousel));

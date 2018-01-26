9a6aa7152a22f5894754fa3c6a7f260be472a4ca
Sagdi Formanov <forsy@Sagdis-MacBook-Pro.local>
2018-01-27 00:16:23 +0500

    imlemented with functions

---
+++ ANSWERS.md
@@ -0,0 +1,4 @@
+## Answers
+1. DOM stands for Document Object Module. DOM is API for HTML documents which can accessed and manipulated from programming languages like Javascript.
+2. Event is some sort of action, such as user interaction with web page (mouse click, scrolling, hovering mouse over, keyboard key press etc) DOM allows Javascript to register such events and pass into functions.
+3. Event listener is DOM method which takes events(actions) as a parameter and perform appointed action(function).
--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,6 +1,22 @@
-class Carousel {
 
+let carouselIndex = 1;
+carousel(carouselIndex);
+
+function plusCarousel(n) {
+  carousel(carouselIndex += n);
 }
 
-let carousels = document.querySelectorAll(".Carousel");
-carousels = Array.from(carousels).map(carousel => new Carousel(carousel));
+function currentCarousel(n) {
+  carousel(carouselIndex = n);
+}
+
+function carousel(n) {
+  let i;
+  const slides = document.getElementsByClassName('Carousel__item');
+  if (n > slides.length) {carouselIndex = 1}
+  if (n < 1) {carouselIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = 'none';
+  }
+  slides[carouselIndex - 1].style.display = " inline-block";
+}

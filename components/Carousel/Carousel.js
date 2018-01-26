3982f60306b9e80d9af78eb915f8ac1a3f17ce40
Nikhil Kamineni <nikhil@nk-mbp.local>
2018-01-26 12:50:48 -0500

    Fixed some typos in index.html; started wokring on carousel.js

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,5 +1,24 @@
 class Carousel {
+  constructor(element) {
+    this.element = element;
+    this.carItems = element.querySelectorAll(".Carousel__item");
+    this.carArrowLeft = element.querySelectorAll(".Carousel__arrow-left");
+    this.carArrowRight = element.querySelectorAll(".Carousel__arrow-right");
+    this.items = Array.from(this.carItems).map((item) => {
+      return new CarItem(item);
+    });
+    console.log(this.element);
+    console.log(this.carItems);
+    console.log(this.ArrowLeft);
+    console.log(this.ArrowRight);
+    this.activeItem = this.carItems[0];
+  }
+}
 
+class carItem {
+  constructor(element) {
+
+  }
 }
 
 let carousels = document.querySelectorAll(".Carousel");

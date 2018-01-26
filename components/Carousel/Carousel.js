3b86556a660f8b12ddbf6d8b73af19f97eab487a
Eric <erichech@gmail.com>
2018-01-26 14:10:13 -0500

    blegh

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,5 +1,62 @@
-class Carousel {
+class CarouselItem {
+	constructor(element) {
+		this.element = element;
+	}
 
+	select() {
+		this.element.classList.add("Carousel__item-focused");
+	}
+
+	deselect() {
+		this.element.classList.remove("Carousel__item-focused");
+	}
+}
+
+class Carousel {
+  constructor(element) {
+    this.element = element;
+    this.items = element.querySelectorAll(".Carousel__items");
+    this.items = Array.from(this.items).map(item => {
+      return new CarouselItem(item);
+		});
+
+		let length = this.items.length;
+		let i = 0;
+    document.addEventListener("click", () => {
+      if (event.target.dataset.arrow) {
+				const data = event.target.dataset.arrow;
+				console.log(data);
+				if (data === 'R') {
+					if (i < length) i++;
+					else {
+						i = 0;
+					}
+					this.updateActive(this.items[i]);
+				}
+				if (data === 'L' && i > length) {
+					if (i > length) i--;
+					else {
+						i = length;
+						i--;
+					}
+					this.updateActive(this.items[i]);
+				}
+    	}
+		});
+
+		this.active = this.items[i];
+		this.init();
+	}
+
+	init() {
+		this.active.select();
+	}
+
+	updateActive(newActive) {
+		this.active.deselect();
+		this.active = newActive;
+		this.active.select();
+	}
 }
 
 let carousels = document.querySelectorAll(".Carousel");

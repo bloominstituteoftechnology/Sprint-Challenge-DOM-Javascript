8e5e540831066719ce14f8431165485230c3b77f
Nikhil Kamineni <nikhil@nk-mbp.local>
2018-01-26 13:24:17 -0500

    Got detection of right and left arrow clicks working

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -2,20 +2,24 @@ class Carousel {
   constructor(element) {
     this.element = element;
     this.carItems = element.querySelectorAll(".Carousel__item");
-    this.carArrowLeft = element.querySelectorAll(".Carousel__arrow-left");
-    this.carArrowRight = element.querySelectorAll(".Carousel__arrow-right");
     this.items = Array.from(this.carItems).map((item) => {
       return new CarItem(item);
     });
-    console.log(this.element);
-    console.log(this.carItems);
-    console.log(this.ArrowLeft);
-    console.log(this.ArrowRight);
+    // console.log(this.element);
+    // console.log(this.carItems);
     this.activeItem = this.carItems[0];
+    this.element.addEventListener('click', (event) => {
+      // let carArrowLeft = this.element.querySelector(".Carousel__arrow-left");
+      // let carArrowRight = this.element.querySelector(".Carousel__arrow-right");
+      let carArrowLeftClicked = event.target.classList[0] === 'Carousel__arrow-left'; 
+      let carArrowRightClicked = event.target.classList[0] === 'Carousel__arrow-right';
+      if (carArrowLeftClicked) console.log('Clicked left arrow');
+      if (carArrowRightClicked) console.log('Clicked right arrow');
+    })
   }
 }
 
-class carItem {
+class CarItem {
   constructor(element) {
 
   }

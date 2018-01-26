5bbbd4372275629aadae98a6952017e24b7f54eb
Lo Saephan <teamsky916@gmail.com>
2018-01-26 10:50:16 -0800

    sprint-dom-js finished

---
+++ ANSWERS.md
@@ -0,0 +1,8 @@
+## Questions
+1. What is the DOM?
+2. What is an event?
+2. What is an event listener?
+
+1. DOM stands for Document Object Model. It is what you see on the broswer window.
+2. Event is a user interaction, such as click, keydown, keyup etc.
+3. Even listener is a method where you can attache to a html node and initialize an event, it'll listen to any event that happens on the lower nodes and can stop the event from bubbling up.
--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,5 +1,45 @@
 class Carousel {
+	constructor(mainElement){
+		// assign parent/main element
+		this.mainElement = mainElement;
+		// retrieve carousel item
+		this.viewItems = this.mainElement.querySelectorAll('.Carousel__item');
+		// assign left arrow node to click event
+		this.leftArrow = this.mainElement.querySelector('.Carousel__arrow-left').addEventListener('click', (event) => {
+			this.updateCarousel(event);
+      event.stopPropagation();
+   	});
+		// assign right arrow node to click event
+		this.rightArrow = this.mainElement.querySelector('.Carousel__arrow-right').addEventListener('click', (event) => {
+			this.updateCarousel(event);
+      event.stopPropagation();
+   	});
+   	// assign current focused item to 0 for tracking
+   	this.currentFocused = 0;
+	}
 
+	// update carousel method
+	updateCarousel(event){
+		if(event.target.dataset.arrow === 'right' && this.currentFocused === 2){
+			this.viewItems[this.currentFocused].classList.remove('Carousel__item-focused');
+			this.currentFocused = 0;
+			this.viewItems[this.currentFocused].classList.add('Carousel__item-focused');
+		}else if(event.target.dataset.arrow === 'right' && this.currentFocused < this.viewItems.length - 1){
+			this.viewItems[this.currentFocused].classList.remove('Carousel__item-focused');
+			this.currentFocused++;
+			this.viewItems[this.currentFocused].classList.add('Carousel__item-focused');
+		}
+
+		if(event.target.dataset.arrow === 'left' && this.currentFocused === 0){
+			this.viewItems[this.currentFocused].classList.remove('Carousel__item-focused');
+			this.currentFocused = 2;
+			this.viewItems[this.currentFocused].classList.add('Carousel__item-focused');
+		}else if(event.target.dataset.arrow === 'left' && this.currentFocused > 0){
+			this.viewItems[this.currentFocused].classList.remove('Carousel__item-focused');
+			this.currentFocused--;
+			this.viewItems[this.currentFocused].classList.add('Carousel__item-focused');
+		}
+	}
 }
 
 let carousels = document.querySelectorAll(".Carousel");

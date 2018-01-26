a72b43e7e20c0a53b86b14eebf5bfc5c42de63f8
Amanda Phillips <amanda@phillipsdev.org>
2018-01-26 12:55:23 -0600

    Answers done, incomplete functionality.

---
+++ ANSWERS.md
@@ -0,0 +1,5 @@
+## Questions
+
+1. DOM - Document Object Model. 
+2. An event is a generated action such as a mouse click or keyboard action.
+3. An event listener is a something that waits for an event to happen to activate certain actions after the event takes place.
--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,5 +1,48 @@
-class Carousel {
+class CarouselItem {
+    constructor (element) {
+        this.element = element;
+    }
 
+    select() {
+        this.element.classList.add('Carousel__item-focused');
+    }
+
+    deselect() {
+        this.element.classList.remove('Carousel__item-focused');
+    }
+}
+
+class Carousel {
+    constructor (element) {
+        this.element  = element;
+        this.items = element.querySelectorAll('.Carousel__item');
+        this.items = Array.from(this.items).map((item) => {
+            return new CarouselItem(item);
+        });
+
+        this.arrowLeft = element.querySelector('.Carousel__arrow-left');
+        this.arrowRight= element.querySelector('.Carousel__-arrow-right');
+
+        this.arrowLeft.addEventListener('click', (event) => {
+            this.updateActive('left');
+        });
+
+
+    this.activeData = 0;
+    }
+
+    updateActive (arrow) {
+        if (arrow === null) return;
+        this.items[this.activeData].deselect();
+        if (arrow === 'left') {
+            if (this.activeData === 0) this.activeData = 3;
+            this.items[--this.activeData].select();
+        }
+        if (arrow === 'right') {
+            if (this.activeData === 2) this.activeData = -1;
+            this.items[++this.activeData].select();
+        }
+    }
 }
 
 let carousels = document.querySelectorAll(".Carousel");

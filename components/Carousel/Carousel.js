017a21f6edf33ded85aede445b30ee1ea5e87327
Dixie Korley <dixiekorley@gmail.com>
2018-01-26 14:06:08 -0500

    Incomplete.

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,6 +1,29 @@
 class Carousel {
+    constructor(element) {
+        this.element = element;
+    }
+
+    select() {
+        this.element.classList.add("Carousel__item");
+    }
+
+    deselect() {
+        this.element.classList.remove("Carousel__item");
+      }
+}
+
+class CarouselArrows {
+    constructor(element) {
+        this.element = element;
+        this.element.addEventListener = ('click', event => {
+
+        })
+    }
+   
 
 }
+
+  
 
 let carousels = document.querySelectorAll(".Carousel");
 carousels = Array.from(carousels).map(carousel => new Carousel(carousel));

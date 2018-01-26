b254b813e24af49e2a6c3d675f260d79c359e7a7
ashleijo <shuryjo@gmail.com>
2018-01-26 16:05:42 -0500

    ic

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,3 +1,18 @@
+class CarouselItem  {
+  constructor(element) {
+    this.element = element;
+  }
+
+  select() {
+    this.element.classList.add("Carousel__item-focused");
+  }
+
+  deselect() {
+    this.element.classList.remove("Carousel__item-focused");
+  }
+
+}
+
 class Arrows {
   constructor(element) {
     this.element = element;
@@ -6,12 +21,37 @@ class Arrows {
     });
   }
 
-  select() {
-    console.log('sel');
-  }
-
-  deselect() {
-    console.log('del');
+  change(x) {
+    if (x == 1) {
+      console.log('prev');
+      var cItems = document.getElementsByClassName("Carousel__item");
+      for (let i = 0; i < cItems.length; i++) {
+        if (cItems[i].classList.contains("Carousel__item-focused")) {
+          cItems[i].classList.remove("Carousel__item-focused");
+          if (i === 0) {
+            i = cItems.length - 1;
+          } else {
+            i--;
+          }
+          console.log(i)
+          cItems[i].classList.add("Carousel__item-focused");
+        }
+      }
+  } else if (x == 2) {
+      console.log('next');
+      var cItems = document.getElementsByClassName("Carousel__item");
+      for (let i = 0; i < cItems.length; i++) {
+        if (cItems[i].classList.contains("Carousel__item-focused")) {
+          cItems[i].classList.remove("Carousel__item-focused");
+          if (i < cItems.length - 1) {
+            i++;
+          } else {
+            i = 0;
+          }
+          cItems[i].classList.add("Carousel__item-focused");
+        }
+      }
+    }
   }
 }
 
@@ -19,30 +59,31 @@ class Carousel {
   constructor(element) {
     this.element = element;
 
+    this.arrows = element.querySelectorAll(".Carousel__arrow-left, .Carousel__arrow-right");
+    this.arrows = Array.from(this.arrows).reduce((obj, item) => {
+      obj[item.dataset.dir2] = new Arrows(item);
+      return obj;
+    }, {});
+
     this.items = element.querySelectorAll(".Carousel__item");
     this.items = Array.from(this.items).reduce((obj, item) => {
-      obj[item.dataset.dir] = new Arrows(item);
+      obj[item.dataset.dir] = new CarouselItem(item);
       return obj;
     }, {});
 
     this.element.addEventListener('click', (event) => {
       if (event.arrowDirection) {
-      console.log('click')
-       this.whichWay(event.arrowDirection);
-       event.stopPropagation();
+        this.whichWay(event.arrowDirection);
+        event.stopPropagation();
       }
     })
 
     this.activeData = element.querySelector(".Carousel__item-focused");
     this.activeData = this.activeData ? this.activeData.dataset.dir : null;
-    this.whichWay(this.activeData);
   }
 
   whichWay(data) {
-    if (data === null) return;
-    this.items[this.activeData].deselect();
-    this.items[data].select();
-    this.activeData = data;
+    this.arrows[data].change(data);
   }
 }
 

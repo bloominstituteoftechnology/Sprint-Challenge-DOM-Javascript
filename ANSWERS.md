0057a8ba21278e521003282361d36c79f9b2f418
lambda <lambda@aegis.local>
2018-01-26 13:12:58 -0600

    DOM Sprint Challenge Progress

---
+++ ANSWERS.md
@@ -0,0 +1,12 @@
+1. What is the DOM? 
+
+The DOM is an interface that allows a programming language to manipulate and dynamically access and update the content, structure, and style of a website. 
+
+2. What is an event?
+
+An event is an action that occurrs that allows for some other action to activate. For example, when a user hovers over an item or when the web page loads. 
+
+3. What is an event listener?
+
+An event listener is a function that listens for and reacts to an event. 
+
--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,6 +1,31 @@
-class Carousel {
+// my mind is melting
 
+class Carousel {
+  constructor(element) {
+    this.element = element;
+    this.classList = element.classList;
+  }
+
+  select() {
+    this.classList.add('Carousel__item-focused');
+  }
+
+  deselect() {
+    this.classList.remove('Carousel__item-focused');
+  }
 }
+
+class Arrows {
+  constructor(element) {
+    this.element = element;
+
+    }
+  }
+
+
+
+
+
 
 let carousels = document.querySelectorAll(".Carousel");
 carousels = Array.from(carousels).map(carousel => new Carousel(carousel));

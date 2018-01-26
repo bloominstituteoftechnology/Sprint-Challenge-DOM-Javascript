1ce8c7e7011d6032b25daa2c39aa2c78b0f54aed
ashleijo <shuryjo@gmail.com>
2018-01-26 14:33:57 -0500

    sprint challenge

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,5 +1,49 @@
-class Carousel {
+class Arrows {
+  constructor(element) {
+    this.element = element;
+    this.element.addEventListener('click', (event) => {
+      event.arrowDirection = this.element.dataset.dir2;
+    });
+  }
 
+  select() {
+    console.log('sel');
+  }
+
+  deselect() {
+    console.log('del');
+  }
+}
+
+class Carousel {
+  constructor(element) {
+    this.element = element;
+
+    this.items = element.querySelectorAll(".Carousel__item");
+    this.items = Array.from(this.items).reduce((obj, item) => {
+      obj[item.dataset.dir] = new Arrows(item);
+      return obj;
+    }, {});
+
+    this.element.addEventListener('click', (event) => {
+      if (event.arrowDirection) {
+      console.log('click')
+       this.whichWay(event.arrowDirection);
+       event.stopPropagation();
+      }
+    })
+
+    this.activeData = element.querySelector(".Carousel__item-focused");
+    this.activeData = this.activeData ? this.activeData.dataset.dir : null;
+    this.whichWay(this.activeData);
+  }
+
+  whichWay(data) {
+    if (data === null) return;
+    this.items[this.activeData].deselect();
+    this.items[data].select();
+    this.activeData = data;
+  }
 }
 
 let carousels = document.querySelectorAll(".Carousel");

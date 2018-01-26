0d274733be2e4cc97fc353613ffb885209681217
Michael Marshal <mikemarshalman@gmail.com>
2018-01-26 14:20:40 -0600

    Fixed Push

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,11 +1,11 @@
 class Carousel {
   constructor(element) {
     // using the tab node
+    this.links = element.querySelectorAll(".Carousel__item");
+    this.links = Array.from(this.links);
+    console.log(this.links);
     this.element = element; // attaches the dom node to the object as "this.element"
     this.pageSetting = 1;
-    this.firstBox = element.querySelector(".Box--grey");
-    this.secondBox = element.querySelector(".Box--blue");
-    this.thirdBox = element.querySelector(".Box--green");
     this.leftArrow = element.querySelector(".Carousel__arrow-left");
     this.rightArrow = element.querySelector(".Carousel__arrow-right");
     this.leftArrow.addEventListener('click', (e) => {
@@ -24,34 +24,39 @@ class Carousel {
         this.pageSetting++;
       }
       console.log('you clicked the right arrow: ' + this.pageSetting);
+      this.switchEmRight();
     });
-    this.switchEmRight();
   }
   switchEmLeft() {
     switch(this.pageSetting) {
       case 1:
-      this.firstBox.classList.remove("Carousel__item-focused");
-      this.firstBox.classList.add("Carousel__item");
-      this.thirdBox.classList.remove("Carousel__item");
-      this.thirdBox.classList.add("Carousel__item-focused");
+      this.links[1].classList.remove("Carousel__item-focused");
+      this.links[0].classList.add("Carousel__item-focused");
       break;
       case 2:
-      this.thirdBox.classList.remove("Carousel__item-focused");
-      this.thirdBox.classList.add("Carousel__item");
-      this.secondBox.classList.remove("Carousel__item");
-      this.secondBox.classList.add("Carousel__item-focused");
+      this.links[2].classList.remove("Carousel__item-focused");
+      this.links[1].classList.add("Carousel__item-focused");
       break;
       case 3:
-      this.thirdBox.classList.remove("Carousel__item-focused");
-      this.secondBox.classList.add("Carousel__item-focused");
+      this.links[0].classList.remove("Carousel__item-focused");
+      this.links[2].classList.add("Carousel__item-focused");
       break;
     }
   }
   switchEmRight() {
     switch(this.pageSetting) {
       case 1:
-      this.firstBox.classList.remove("Carousel__item-focused");
-      this.secondBox.classList.add("Carousel__item-focused");
+      this.links[2].classList.remove("Carousel__item-focused");
+      this.links[0].classList.add("Carousel__item-focused");
+      break;
+      case 2:
+      this.links[0].classList.remove("Carousel__item-focused");
+      this.links[1].classList.add("Carousel__item-focused");
+      break;
+      case 3:
+      this.links[1].classList.remove("Carousel__item-focused");
+      this.links[2].classList.add("Carousel__item-focused");
+      break;
     }
   }
 }

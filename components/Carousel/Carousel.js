dc78d448f11071c1d0633827d97ec529479fbbe8
Res Yeg <shaqk702@gmail.com>
2018-01-26 11:07:25 -0800

    sprint challenge submission

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,4 +1,12 @@
 class Carousel {
+    constructor(element) {
+        this.element = element;
+        this.items = element.querySelectorAll(".Carousel__item");
+        this.items = Array.from(this.items);
+
+        this.arrows = element.querySelectorAll(`.Carousel__Arrow-${direction}`);  // ?
+        this.arrows = Array.from(this.arrows);
+    // i'm lost
 
 }
 

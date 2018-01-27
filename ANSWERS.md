7e8a8c080de92613b5eca990ae915c8bf89c963e
aceredesign <steven.magadan@hotmail.com>
2018-01-26 16:36:32 -0800

    completed

---
+++ ANSWERS.md
@@ -0,0 +1,17 @@
+## What is the DOM?
+
+The document object model is an interface for HTML to communicate with programming languages. This allows the programming language to connect the page and make changes to the way the page functions and displays information.
+
+## What is an event?
+
+This is when something occurs on the page, such as finishing loading, modifying an input field, or a button being clicked.
+
+**Some examples include**
+
+* onclick
+* onkeydown
+* onload
+
+## What is an event listener?
+
+A function or loop programmed to perform an operation.
--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,6 +1,71 @@
-class Carousel {
+class CarouselItem {
+  constructor(carousel) {
+    this.carousel = carousel;
+  }
 
+  select() {
+    this.carousel.classList.add("Carousel__item-focused");
+  }
+
+  deselect() {
+    console.log(this.carousel);
+    this.carousel.classList.remove("Carousel__item-focused");
+  }
+
+  getDataIndex() {
+    return this.carousel.dataset.tab;
+  }
 }
 
-let carousels = document.querySelectorAll(".Carousel");
-carousels = Array.from(carousels).map(carousel => new Carousel(carousel));
+class CarouselController {
+  constructor(carousels) {
+    this.carousels = carousels;
+    console.log(carousels);
+    this.currentIndex = 0;
+    this.activeCarousel = this.carousels[this.currentIndex];
+    this.carouselLeftArrow = document.querySelector(".Carousel__arrow-left");
+    this.carouselRightArrow = document.querySelector(".Carousel__arrow-right");
+    this.carouselLocationElement = document.querySelector(
+      ".Carousel__location"
+    );
+    this.bindCarouselClickEvents();
+    this.setCarouselLocationElement();
+  }
+
+  getCarousel(index) {
+    return this.carousels[index];
+  }
+
+  setActiveCarousel(carousel) {
+    this.activeCarousel.deselect();
+    this.activeCarousel = carousel;
+    this.activeCarousel.select();
+  }
+
+  setCarouselLocationElement() {
+    this.carouselLocationElement.textContent = `${this.currentIndex + 1} / ${
+      this.carousels.length
+    }`;
+  }
+
+  bindCarouselClickEvents() {
+    this.carouselLeftArrow.addEventListener("click", () => this.moveLeft());
+    this.carouselRightArrow.addEventListener("click", () => this.moveRight());
+  }
+
+  moveLeft() {
+    if (this.currentIndex === 0) return;
+    this.setActiveCarousel(this.getCarousel(--this.currentIndex));
+    this.setCarouselLocationElement();
+  }
+
+  moveRight() {
+    if (this.currentIndex === this.carousels.length - 1) return;
+    this.setActiveCarousel(this.getCarousel(++this.currentIndex));
+    this.setCarouselLocationElement();
+  }
+}
+
+let carousels = document.querySelectorAll(".Carousel__item");
+carousels = Array.from(carousels).map(carousel => new CarouselItem(carousel));
+let carouselController = new CarouselController(carousels);

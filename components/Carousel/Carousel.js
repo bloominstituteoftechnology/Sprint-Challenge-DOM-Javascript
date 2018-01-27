5a1efc3cd4c6f096f20862a7ec2ce3647b98cdb6
algorithmsrock <algorithmsrock@gmail.com>
2018-01-26 16:36:33 -0800

    Sprint-Challenge-DOM-Javascript completed with bugs fixed

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -2,12 +2,12 @@
      constructor(element) {
 		  this.element = element;
       this.carouselitems = element.querySelectorAll(".Carousel__item");
-      this.activecarouselItem = this.carouselitems[0];
+      this.activeCarouselItem = this.carouselitems[0];
       this.activeCarouselIndex = 0;
        this.left = element.querySelector(".Carousel__arrow-left");
        this.right = element.querySelector(".Carousel__arrow-right");
        this.left.addEventListener("click", (event) => {
-				 this.updateActive("right");
+				 this.updateActive("left");
 				 });
 
 			 this.right.addEventListener("click", (event) => {
@@ -16,39 +16,6 @@
 
 	}
 
-/*class CarouselArrowLeft { 
-	 constructor(element) {
-		 this.element = element;
-		 this.carouselitems = element.querySelectorAll(".Carousel__item");
-     this.activecarouselItem = this.caraouselitem[0];
-		 this.activeCarouselIndex = 0;
-
-		 this.left = element.querySelector(".Carousel__arrow-left");
-		 this.left.addEventListener('click',(event) => {
-			 this.updateActive("left");
-     }
-	} 
-	}
- class CarouselArrowRight {
-	 constructor(element) {
-		 this.element = element;
-		 this.right = newCarouselArrowRight(this.element.querySelector(".Carousel__arrow-right");
-		 this.right.addEventListener('click', (event) => {
-			 this.updateActive("right");
-  }
-}*/
- /*class Carousel {
-	 constructor(element) {
-	  this.element = element;
-		this.carouselitems = element.querySelectorAll(".Carousel__item");
-	  this.activecarouselItem = this.caraouselitem[0];
-		this.activeCarouselIndex = 0;
-		 this.left = element.querySelector(".Carousel__arrow-left");
-				this.updateActive("left");
-		 this.right = newCarouselArrowRight(this.element.querySelector(".Carousel__arrow-right");
-        this.updateActive("right");
-		}
-}*/
   updateActive(CarouselArrow) {
 	  if (CarouselArrow === null)
 		 return;
@@ -57,7 +24,7 @@
 
 		 if (CarouselArrow === "left") {
 		   if (this.activeCarouselIndex === 0) {
-			    this.activeCarouselIndex = this.carouselitems.length - 1; {
+			    this.activeCarouselIndex = this.carouselitems.length - 1; 
 				}
 			  else {
 					this.activeCarouselIndex--;
@@ -65,7 +32,7 @@
 
 			}
       else if (CarouselArrow === "right") {
-				if (this.activeCarouselIndex = this.carouselitems.length - 1) {
+				if (this.activeCarouselIndex === this.carouselitems.length - 1) {
 					this.activeCarouselIndex = 0;
 				}
 				 else {
@@ -77,11 +44,11 @@
 		}
 
       select() {
-       this.activeCarouselItem.classList.add("Carousel__item-selected");
+       this.activeCarouselItem.classList.add("Carousel__item-focused");
       }
   
      deselect() {
-      this.activeCarouselItem.classList.remove("Carousel__item-selected");
+      this.activeCarouselItem.classList.remove("Carousel__item-focused");
     }
   }
 

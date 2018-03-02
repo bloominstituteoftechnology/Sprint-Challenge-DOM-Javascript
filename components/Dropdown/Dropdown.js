2e4d967172c136784282eb62566ff156eeda4103
Erik Alonso <mr.alonsoea@protonmail.ch>
2018-03-02 15:09:04 -0600

    modified dropdown

--- components/Dropdown/Dropdown.js
+++ components/Dropdown/Dropdown.js
@@ -1,15 +1,20 @@
 class Dropdown {
   constructor(element) {
-    this.element = element;
-    this.button = this.element.querySelector(".Dropdown__button");
+    this.element = element; // ARRAY PASSES Dropdown__button AND Dropdown__content
+    this.button = this.element.querySelector(".Dropdown__button"); // THIS METHOD RETURNS Dropdown__button for Dropdown__button and NULL for Dropdown__content
 
-    this.button.addEventListener("click", () => {
+    this.button.addEventListener("click", () => { // ADDS THE EVENT LISTENER TO this.button or Dropdown__button ONLY
       this.toggleContent();
+      this.greyOut();
     });
   }
 
   toggleContent() {
     this.element.classList.toggle("Dropdown--active");
+  }
+
+  greyOut() {
+    this.button.classList.toggle("Dropdown--greyed");
   }
 }
 

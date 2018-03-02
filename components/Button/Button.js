96fce770a28df06cb05c603135b3c95ad482a52f
unknown <ruiz.james12@yahoo.com>
2018-03-02 10:35:15 -0800

    James Ruiz Revised

--- components/Button/Button.js
+++ components/Button/Button.js
@@ -2,12 +2,9 @@ class Buttons {
     constructor(element) {
       this.element = element;
       this.element.addEventListener("click", (element) => {
-        this.makeMagic();
-      });
-}
-
-    makeMagic() {
         this.element.classList.add("Button__magic")
+      });
+    }
 }
 
 let buttons = document.getElementsByClassName("Button");

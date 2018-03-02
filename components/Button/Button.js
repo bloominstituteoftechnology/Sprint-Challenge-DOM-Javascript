26f17f3735ecbdf25f4e37e9e362c99b97404742
unknown <jacobiswinning@gmail.com>
2018-03-02 13:29:29 -0700

    added code that works!

--- components/Button/Button.js
+++ components/Button/Button.js
@@ -1,19 +1,10 @@
 class Button {
-    constructor(element) {
+    constructor(element){
         this.element = element;
-        this.button = this.element.querySelector(".Button");
-
-        this.button.addEventListener("click", () => {
-        this.buttonDisp();
+        this.element.addEventListener('click', (element) => {
+            this.element.classList.add("buttonClicked");
         });
     }
-    buttonDisp() {
-        let div = document.getElementsByClassName("Button");
-        div.style.display = 'none';
-        // this.element = document.getElementsByClassName("Button");
-        // this.element.style.display = 'none';
-        console.log('This will never be seen because you suck at Javascript!!!');
-    }
 }
-let buttons = document.querySelectorAll("Button");
-buttons = Array.from(buttons).map(button => new Button(button));
+let buttons = document.getElementsByClassName("Button");
+buttons = Array.from(buttons).map(buttons => new Button(buttons));

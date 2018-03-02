6b40e57281e9a7a955b1268e1f5d57dc60c47c84
Charletta <charlettabullard12@outlook.com>
2018-03-02 18:28:55 -0500

    Complete

--- components/Button/Button.js
+++ components/Button/Button.js
@@ -1,24 +1,13 @@
-//Initiate the Constructor
 class Button {
   constructor(element) {
-    this.element = element;
-    //Calling the addEventListener function
-    this.element.addEventListener('click', () => {
-    this.element.classList.add('Button--hidden');
-      //Setting the toggle to true
-    // this.element.onClick === 'hidden';
-    })
+      this.element = element;
+      this.element.addEventListener("click", ()  => {
+          this.element.classList.add("Button--hidden");
+      });
   }
-//Setting up my function
-// this.element = () => {
-//          if document.getElementByClassName("Button").onclick () => {
-//               document.getElementByClassName("Button").style.visibility === "hidden";} else {
-//               document.getElementByClassName("Button").style.visibility === "visible";
-//             }
-//         }
-//     }
+}
 
-let buttons = document.getElementByClassName('.Button');
+let buttons = document.getElementsByClassName("Button");
 buttons = Array.from(buttons).map(button => {
-  return new Button(button);
+  return new Button(button)
 });

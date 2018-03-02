fc6716a3daed6bdd8de556562f050d5c3ad80a69
Charletta <charlettabullard12@outlook.com>
2018-03-02 18:21:05 -0500

    WIP more update to the button.js and button.css

--- components/Button/Button.js
+++ components/Button/Button.js
@@ -1,17 +1,24 @@
 //Initiate the Constructor
 class Button {
-  constructor(hidButton) {
-    this.hidButton = hidButton;
+  constructor(element) {
+    this.element = element;
+    //Calling the addEventListener function
+    this.element.addEventListener('click', () => {
+    this.element.classList.add('Button--hidden');
+      //Setting the toggle to true
+    // this.element.onClick === 'hidden';
+    })
   }
+//Setting up my function
+// this.element = () => {
+//          if document.getElementByClassName("Button").onclick () => {
+//               document.getElementByClassName("Button").style.visibility === "hidden";} else {
+//               document.getElementByClassName("Button").style.visibility === "visible";
+//             }
+//         }
+//     }
 
-//Function used to create the event
-const hidButton = document.getElementsByClassName('Button');
-
-hidButton.onClick = function() {
-
-        if(this.hidButton === hidden) {
-            document.getElementByClassName('Button').style.display = 'none';
-        } else {
-            document.getElementByClassName('Button').style.display = 'visible';
-        }
-    }
+let buttons = document.getElementByClassName('.Button');
+buttons = Array.from(buttons).map(button => {
+  return new Button(button);
+});

afd55d554abb0b2f5790f636fb2f7c9246a7655d
kait-schorr <kaitlynschorr@gmail.com>
2018-03-02 11:45:31 -0600

    Created class constructors and working on the arrow functionality now

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,5 +1,23 @@
 class Carousel {
+    constructor(element) {
+        this.element = element;
+        this.leftArrow = document.getElementsByClassName("Carousel__arrow-left")[0];
+        this.rightArrow = document.getElementsByClassName("Carousel__arrow-right")[0];
+        this.items = element.getElementsByClassName("Carousel__item");
+        this.items = Array.from(this.items).map(item => new CarouselItem(item));
+    }
+}
 
+class CarouselItems {
+    constructor(element){
+        this.element = element;
+    }
+}
+
+class CarouselItem {
+    constructor(element) {
+        this.element = element;
+    }
 }
 
 let carousels = document.querySelectorAll(".Carousel");

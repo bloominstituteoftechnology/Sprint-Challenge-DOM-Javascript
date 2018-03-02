6796d689d129dcc145eb72b91c9eae45621c24b1
Juber Ahmed <a.juber@hotmail.com>
2018-03-02 12:28:35 -0500

    Completed Diappearing Buttons with JavaScript

--- components/Button/Button.js
+++ components/Button/Button.js
@@ -1,18 +1,17 @@
 class Button {
     constructor(element) {
         this.element = element;
-        this.button = this.element.querySelector(".Button");
-   
-        // this.button.addEventListener("click", () => {
-        //     this.button.deselect();
-        //     console.log('ev');
-        // }); 
-    }
-    
-    deselect() {
-        this.element.classList.remove("Button");
+
+        //console.log('1')
+        this.element.addEventListener("click", () => {
+            this.element.classList.add("Button--selected");
+            //console.log('clicked');
+        });
     }
 }
 
 let buttons = document.querySelectorAll(".Button");
-buttons = Array.from(buttons).map(buttons => new Button(buttons));
+buttons = Array.from(buttons).map(buttonDisappear => {
+    new Button(buttonDisappear)
+   // console.log('err');
+});

74db04b3fdf6f9bcd43e0b8ebed11a359c98d8f2
Christopher Atoki <kingatoki@Christophers-MacBook-Air.local>
2018-03-02 11:28:56 -0500

    Revert

--- components/Button/Button.js
+++
@@ -1,6 +0,0 @@
-let buttons = document.getElementsByClassName("Button");
-buttons = Array.from(buttons).map(button => {
-    button.addEventListener("click", () => {
-        button.classList.add("Button-hidden");
-    })
-})

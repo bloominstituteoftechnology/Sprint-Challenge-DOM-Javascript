3d4bf8482839a925c579053d6bfe5e3eb3a82ef8
Megan Williamson <meganawilliamson@gmail.com>
2018-03-02 09:09:41 -0800

    Button constructor created, buttons hide

--- components/Button/Button.js
+++ components/Button/Button.js
@@ -1,2 +1,17 @@
-let buttons = document.querySelectorAll(".button");
-buttons = Array.from(buttons).map(button => new Button(button));
+class Button {
+    constructor(element) {
+      this.element = element;
+    //   this.button = this.element.querySelector(".Dropdown__button");
+  
+      this.element.addEventListener("click", () => {
+          this.hideContent();
+      });
+    }
+  
+    hideContent() {
+      this.element.classList.toggle("Button--hidden");
+    }
+  }
+  
+  let buttons = document.querySelectorAll(".Button");
+  buttons = Array.from(buttons).map(button => new Button(button));

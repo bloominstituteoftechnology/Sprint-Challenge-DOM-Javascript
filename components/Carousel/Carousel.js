f97438773e3bad41dc4027fbb4cb54288ca0c3cf
Kevin Jolley <30558808+Serrowxd@users.noreply.github.com>
2018-03-02 15:46:37 -0600

    Additions

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -13,23 +13,23 @@ class Carousel {
         this.element = element;
 
         this.carouselItems = this.element.querySelectorAll(".Carousel__item");
-        this.carouselItems = Array.from(this.carouselItems).map(item => new CarouselItems(item));
+        this.carouselItems = Array.from(this.carouselItems).map(item => new CarouselItem(item));
         this.carouselItems.unshift(this.carouselItems.pop());
         this.focusIndex = Math.ceil((this.carouselItems.length / 2) - 1);
 
-        this.rightArrow = this.element.querySelectorAll(".Carousel__arrow-right");
-        this.leftArrow = this.element.querySelectorAll(".Carousel__arrow-left");
+        this.rightArrow = this.element.querySelector(".Carousel__arrow-right");
+        this.leftArrow = this.element.querySelector(".Carousel__arrow-left");
         this.rightArrow.addEventListener("click",() => {
             this.switchRight();
         });
-        this.leftArrow.addEventListener("click", () => {
+        this.leftArrow.addEventListener("click",() => {
             this.switchLeft();
         });
     }
 
     // initItems() { // stacks the items logic into an init function
     //     this.carouselItems = this.element.querySelectorAll(".Carousel__item");
-    //     this.carouselItems = Array.from(this.carouselItems).map(item => new CarouselItems(item));
+    //     this.carouselItems = Array.from(this.carouselItems).map(item => new CarouselItem(item));
     //     this.carouselItems.unshift(this.carouselItems.pop());
     //     this.focusIndex = Math.ceil((this.carouselItems.length / 2) - 1);        
     // }

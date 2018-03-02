bd2275ff7ee1c801e61dfc72a14fd53413924ed3
unknown <jacobiswinning@gmail.com>
2018-03-02 12:07:40 -0700

    added a bunch of stuff that does not work

--- components/Button/Button.js
+++ components/Button/Button.js
@@ -1,15 +1,19 @@
 class Button {
     constructor(element) {
         this.element = element;
-        this.button = this.element.querySelector("Button");
-    
+        this.button = this.element.querySelector(".Button");
+
         this.button.addEventListener("click", () => {
-          this.buttonDisp(button);
+        this.buttonDisp();
         });
-      }
-      buttonDisp(button) {
-        var div = document.getElementsByClassName("Button");
+    }
+    buttonDisp() {
+        let div = document.getElementsByClassName("Button");
         div.style.display = 'none';
+        // this.element = document.getElementsByClassName("Button");
+        // this.element.style.display = 'none';
+        console.log('This will never be seen because you suck at Javascript!!!');
     }
 }
-    
+let buttons = document.querySelectorAll("Button");
+buttons = Array.from(buttons).map(button => new Button(button));

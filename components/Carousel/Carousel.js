3f148af2a148f32efe1f2273263dc83077e3cc6b
AJ Genung <DragonRydrz@me.com>
2018-03-02 13:59:18 -0500

    working on carousel

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -4,23 +4,39 @@ class CarouselItem {
   }
 
   select() {
-    this.element.classList.add('')
+    this.element.classList.add('Carousel__item-focused')
+  }
+  deselect() {
+    this.element.classList.remove('Carousel__item-focused');
   }
 }
 
 class Carousel {
   constructor(element) {
     this.element = element;
-    this.element.querySelector('.Carousel__arrow-left').addEventListener('click', () => { slideLeft() });
-    this.element.querySelector('.Carousel__arrow-right').addEventListener('click', () => { slideRight() });    
-    this.items = this.element.querySelectorAll('Boxe');
+    this.arrowLeft = this.element.querySelector('.Carousel__arrow-left')
+    this.arrowLeft.addEventListener('click', () => { this.carousel.updateActive(this) });
+    this.arrowRight = this.element.querySelector('.Carousel__arrow-right')    
+    this.arrowRight.addEventListener('click', () => { });
+    this.items = this.element.getElementsByClassName('.Carousel__item');
+    console.log('this.items');
+    console.log(this.items);
+    
     this.items = Array.from(this.items).map(item => new CarouselItem(item));
-    this.activeitem = this.items[0];
+    console.log(this.items);
+    this.activeItem = this.items[0];
+  }
+
+  updateActive(newActive) {
+    this.activeItem.deselect();
+    this.activeItem = newActive;
+    this.activeItem.select();
   }
 
 }
 
 let carousels = document.querySelectorAll(".Carousel");
+console.log('carousels');
 console.log(carousels);
 carousels = Array.from(carousels).map(carousel => new Carousel(carousel));
 console.log(carousels);

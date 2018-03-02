cb1eb97f1c717be7f1e4298b59d38ede03c27b81
Joshua Goode <jgoode.business@gmail.com>
2018-03-02 12:20:17 -0500

    Added basic CSS and JS functionality to Buttons component -- initial challenge completed

--- components/Button/Button.js
+++ components/Button/Button.js
@@ -1,1 +1,18 @@
 /* Sprint-Challenge-DOM-Javascript - Joshua Goode */
+
+class Button {
+    constructor(element) {
+        this.element = element;
+        
+        this.element.addEventListener("click", () => {
+            this.hidden();
+        });
+    }
+
+    hidden() {
+        this.element.classList.add("Button--hide");
+    }
+}
+
+let buttons = document.querySelectorAll(".Button");
+buttons = Array.from(buttons).map(button => new Button(button));

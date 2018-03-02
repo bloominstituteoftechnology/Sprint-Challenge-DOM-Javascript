cee4039ff33b57aa791b68691fb553cfde906fdd
happy <christopherstone@Christophers-MacBook-Pro.local>
2018-03-02 11:40:05 -0500

    Stone Chris Sprint Challenge DOM Javscript
    Boxes Dissapear

---
+++ components/Button/Button.js
@@ -0,0 +1,16 @@
+const Button = Array.from(document.getElementsByClassName('Button'));
+
+Button[0].addEventListener("click", () => {
+    console.log('clicking1');
+    Button[0].style.display="none";
+    });
+
+Button[1].addEventListener("click", () => {
+    console.log('clicking1');
+    Button[1].style.display="none";
+    });
+
+Button[2].addEventListener("click", () => {
+    console.log('clicking1');
+    Button[2].style.display="none";
+    });

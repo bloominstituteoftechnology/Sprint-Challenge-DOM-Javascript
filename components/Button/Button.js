3b7c3ede640a3aba7338582010f139273a42ca1c
Robert Rak <robertrak@hotmail.com>
2018-03-02 12:39:48 -0500

    Added disappearing buttons

---
+++ components/Button/Button.js
@@ -0,0 +1,19 @@
+class Button {
+  constructor(element) {
+    this.element = element;
+
+    this.element.addEventListener('click', () => {
+      this.element.classList.add('Button--hidden');
+    });
+  }
+}
+
+
+
+let button1 = document.getElementsByClassName('Button')[0];
+let button2 = document.getElementsByClassName('Button')[1];
+let button3 = document.getElementsByClassName('Button')[2];
+
+button1 = new Button(button1);
+button2 = new Button(button2);
+button3 = new Button(button3);

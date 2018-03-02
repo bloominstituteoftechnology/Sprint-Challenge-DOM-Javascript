8cc32472234778c3e27053e95d8e2f47a596e4a2
arcadia-ego <arcadia-ego@github.com>
2018-03-02 08:59:44 -0800

    non-stretch goal finished

---
+++ index.js
@@ -0,0 +1,9 @@
+const disButton = document.querySelectorAll(".Button");
+
+disButton.forEach( (item) => {
+
+    item.addEventListener("click", () => {
+        item.classList.toggle("Button--clicked");
+        console.log("test");
+    });
+})

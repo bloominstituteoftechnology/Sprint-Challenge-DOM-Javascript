1876820616764a0273dc7aefc9bf01e801261d76
unknown <ruiz.james12@yahoo.com>
2018-03-02 10:27:32 -0800

    James Ruiz

---
+++ components/Button/Button.js
@@ -0,0 +1,14 @@
+class Buttons {
+    constructor(element) {
+      this.element = element;
+      this.element.addEventListener("click", (element) => {
+        this.makeMagic();
+      });
+}
+
+    makeMagic() {
+        this.element.classList.add("Button__magic")
+}
+
+let buttons = document.getElementsByClassName("Button");
+buttons = Array.from(buttons).map(buttons => new Buttons(buttons));

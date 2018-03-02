805acb05e3d2499ffa76f9294e5a0dec2d49b7af
Robert Rak <robertrak@hotmail.com>
2018-03-02 12:59:48 -0500

    Refactored buttons / started Carousel

--- components/Button/Button.js
+++ components/Button/Button.js
@@ -8,10 +8,5 @@ class Button {
   }
 }
 
-let button1 = document.getElementsByClassName('Button')[0];
-let button2 = document.getElementsByClassName('Button')[1];
-let button3 = document.getElementsByClassName('Button')[2];
-
-button1 = new Button(button1);
-button2 = new Button(button2);
-button3 = new Button(button3);
+let buttons = document.querySelectorAll('.Button');
+buttons = Array.from(buttons).map(button => new Button(button));

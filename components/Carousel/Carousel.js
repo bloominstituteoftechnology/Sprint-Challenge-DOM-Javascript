ce05112ee9d04d89495f109ca8f1210b1795f305
AJ Genung <DragonRydrz@me.com>
2018-03-02 13:15:19 -0500

    changes boxes to CarouselItem

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,9 +1,21 @@
+class CarouselItem {
+  constructor(element) {
+    this.element = element;
+  }
+
+  select() {
+    this.element.classList.add('')
+  }
+}
+
 class Carousel {
   constructor(element) {
     this.element = element;
     this.element.querySelector('.Carousel__arrow-left').addEventListener('click', () => { slideLeft() });
     this.element.querySelector('.Carousel__arrow-right').addEventListener('click', () => { slideRight() });    
-    
+    this.items = this.element.querySelectorAll('Boxe');
+    this.items = Array.from(this.items).map(item => new CarouselItem(item));
+    this.activeitem = this.items[0];
   }
 
 }

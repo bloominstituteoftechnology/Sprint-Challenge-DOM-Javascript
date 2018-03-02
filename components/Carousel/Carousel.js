a48f61ecd82fe4cb4ccc7ddbe22d22d74321a174
Carlos Andrade <carlos.andrade86@outlook.com>
2018-03-02 14:04:02 -0500

    Sprint-Challenge attemp

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,6 +1,46 @@
-class Carousel {
+class Item {
+    constructor(element) {
+        this.element = element;
+    }
+}
 
+
+class RightLink {
+    constructor(element, parent){
+      this.element = element;
+      this.carousels = parent;
+      this.carouselItem = parent.getItem(this.element.dataset.item);
+      this.carouselItem = new Item(this.carouselItem);
+
+    }
+}
+
+class LeftLink {
+    constructor(element, parent){
+      this.element = element;
+      this.carousels = parent;
+    //   this.carouselItem = parent.getItem(this.element.dataset.item);
+    //   this.carouselItem = new Item(this.carouselItem);
+
+    }
+}
+
+class Carousel {
+    constructor(element){
+      this.element = element;
+      this.rightLink = element.querySelector(".Carousel__arrow-right");
+      this.rightLink = new RightLink(this.rightLink, this);
+      this.leftLink = element.querySelector(".Carousel__arrow-left");
+      this.leftLink = new LeftLink(this.leftLink, this);
+      
+    }
+    getItem(data) {
+        return this.element.querySelector(`.Carousel__item[data-item="${data}"]`);
+      }
 }
 
 let carousels = document.querySelectorAll(".Carousel");
 carousels = Array.from(carousels).map(carousel => new Carousel(carousel));
+console.log(carousels);
+
+

28d1949411dc1d67566f0557eafc550ba438c15d
Charletta <charlettabullard12@outlook.com>
2018-03-02 15:55:14 -0500

    WIP...Button.js not disappearing yet

--- components/Button/Button.js
+++ components/Button/Button.js
@@ -1,1 +1,17 @@
+//Initiate the Constructor
+class Button {
+  constructor(hidButton) {
+    this.hidButton = hidButton;
+  }
 
+//Function used to create the event
+const hidButton = document.getElementsByClassName('Button');
+
+hidButton.onClick = function() {
+
+        if(this.hidButton === hidden) {
+            document.getElementByClassName('Button').style.display = 'none';
+        } else {
+            document.getElementByClassName('Button').style.display = 'visible';
+        }
+    }

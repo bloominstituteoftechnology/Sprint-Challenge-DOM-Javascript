5ca81bf0c879984ccd930d9255e46f68cfa89a19
mjd02 <mostdarryl@gmail.com>
2018-03-02 15:36:43 -0500

    finished stretch goal

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,6 +1,38 @@
-class Carousel {
+let right = document.getElementsByClassName("Carousel__arrow-right")[0];
+let left = document.getElementsByClassName("Carousel__arrow-left")[0];
+let activeItem = document.getElementsByClassName("Carousel__item-focused")[0];
+let counter = 0;
 
-}
+right.addEventListener("click",function() {
+	if(parseInt(activeItem.dataset.number) === 2){
+		counter = 0;
+		let current = document.getElementsByClassName("Carousel__item")[counter];
+		activeItem.classList.remove("Carousel__item-focused");
+		current.classList.add("Carousel__item-focused");
+		activeItem = current;
+	}
+	else{
+		counter++;
+		let current = document.getElementsByClassName("Carousel__item")[counter];
+		activeItem.classList.remove("Carousel__item-focused");
+		current.classList.add("Carousel__item-focused");
+		activeItem = current;
+	}
+});
 
-let carousels = document.querySelectorAll(".Carousel");
-carousels = Array.from(carousels).map(carousel => new Carousel(carousel));
+left.addEventListener("click",function() {
+	if(parseInt(activeItem.dataset.number) === 0){
+		counter = 2 
+		let current = document.getElementsByClassName("Carousel__item")[counter];
+		activeItem.classList.remove("Carousel__item-focused");
+		current.classList.add("Carousel__item-focused");
+		activeItem = current;
+	}
+	else{
+		counter--;
+		let current = document.getElementsByClassName("Carousel__item")[counter];
+		activeItem.classList.remove("Carousel__item-focused");
+		current.classList.add("Carousel__item-focused");
+		activeItem = current;
+	}
+});

543e3ce1f10303a24f8e04f811d9a69d1acb1626
dc5stars <dc5stars@yahoo.com>
2018-03-02 12:51:47 -0500

    Dennis Chirchenko. Sprint challenge basic functionality complete and answers done

---
+++ ANSWERS.md
@@ -0,0 +1,7 @@
+1. What is the DOM? DOM stands for Documents Object Model. Simply put just a model that allows you to access HTML objects with javascript and manipulate them all you want.
+
+2. What is an event? An event is a distinct event like a click, mouse button -up or -down, that can trigger a certain action attached to it.
+
+3. What is an event listener? It is a function that links a certain event to a certain function.
+
+4. What is a Constructor. A constructor is a function that allows creation of a class instance. Sort of a template for this purpose.
--- components/Button/Button.js
+++ components/Button/Button.js
@@ -2,7 +2,19 @@ class Button {
     constructor(element) {
         this.element = element;
         
-        this.element.addEventListener("click", () => this.element.classList.add('Button--hidden'));
+        //this.element.addEventListener("click", () => this.element.classList.toggle('Button--hidden'));
+
+        //or with toggle: 
+        this.element.addEventListener("click", () => this.element.classList.toggle('Button--hidden'));
+        //this.element.addEventListener("mouseup", () => this.element.classList.toggle('Button--hidden'));
+        //this.element.classList.toggle('Button--hidden');
+    
+
+
+        //topDropdownList.classList.toggle("selected");
+
+        //this.element.addEventListener("mouseup", () => this.element.classList.remove('Button--hidden'));
+        
     }
 
 }

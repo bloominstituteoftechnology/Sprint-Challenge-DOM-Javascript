3e1169ea3b90d266fd426dd2f67d8c13714f722c
noxasaxon <saxon.h@outlook.com>
2018-03-02 20:57:49 -0800

    Buttons come back after 3.5 sec

--- components/Button/Button.js
+++ components/Button/Button.js
@@ -3,8 +3,12 @@ class Button {
         this.element = element;
         this.element.addEventListener('click', (event) => {
         this.element.style.display = 'none';
+        setTimeout((element) => {
+            this.element.style.display = 'inline-block';
+        }, 3500);
           });
     }
+
 }
 
 let buttons = document.querySelectorAll('.Button');

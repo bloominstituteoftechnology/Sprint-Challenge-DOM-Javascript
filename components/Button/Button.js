f680d089be9d5b9259d4b38b76e3bec032ae5f0c
astro11 <aquila.strother@asu.edu>
2018-03-03 15:47:10 -0500

    refactored - added constructor

--- components/Button/Button.js
+++ components/Button/Button.js
@@ -1,8 +1,26 @@
+/*
 let disappear = document.getElementsByClassName("Button");
 disappear = Array.from(disappear).map(button => {
     button.addEventListener("click", () => {
         button.remove("Button");
     })
 })
+*/
 
+// Class constructor
 
+class Click {
+    constructor(element) {
+        this.element = element;
+
+        this.element.addEventListener("click", () => {
+            this.element.classList.add("Disappear");
+            
+        });
+    }
+}
+
+let buttons = document.getElementsByClassName("Button");
+buttons = Array.from(buttons).map(buttonClick => {
+    new Click(buttonClick)
+});

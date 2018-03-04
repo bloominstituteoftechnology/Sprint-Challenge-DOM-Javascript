7fc431e9fc3b921da5c54fa4b36ae63914a960ea
Courtney Seitz <courtneyelizabethseitz@gmail.com>
2018-03-04 15:48:49 -0500

    added Ivan's solution

--- components/Button/Button.js
+++ components/Button/Button.js
@@ -3,20 +3,25 @@ class Button {
     this.element = element;
     this.element.addEventListener('click', () => {
       this.element.style.display= 'none';
+      //this.element.classList.add('Button--hide');
+      //*Ivan's solution
     })
   }
 }
 
 let buttons = document.getElementsByClassName('Button');
 buttons = Array.from(buttons).map(button => {
-  return new Button(button)
+  return new Button(button);
 });
-
-
+console.log(buttons);
 
 
 // document.getElementsByClassName('Button')[0].addEventListener('click',() => {
 //   document.getElementsByClassName('Button')[0].style.display = 'none';
 // });
-//
-//
+
+
+// *Ivan's solution
+// let buttons = document.querySelectorAll('.Button');
+// buttons = Array.from(buttons).map(button => new Button(button));
+// console.log(buttons);

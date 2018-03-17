2a59c6d452c1cfcbdb74f1d7812a5dd17e638dd1
Shawniqua Greene <shawny.b12@gmail.com>
2018-03-17 13:48:58 -0400

    Shawniqua Greene: WIP

---
+++ components/Button/Button.js
@@ -0,0 +1,15 @@
+let buttonA = document.getElementById('ButtonA');
+let buttonB = document.getElementById('ButtonB');
+let buttonC = document.getElementById('ButtonC');
+
+buttonA.onclick = function() {
+    buttonA.style.visibility = "hidden";
+}
+
+buttonB.onclick = function() {
+    buttonB.style.visibility = "hidden";
+}
+
+buttonC.onclick = function() {
+    buttonC.style.visibility = "hidden";
+}

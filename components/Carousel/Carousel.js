747354240cf339d2687b59f01791d8993d50d59d
Lisa Cee <three_nineteen@yahoo.com>
2018-03-17 11:39:49 -0700

    wip carousel

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,30 +1,37 @@
 class Carousel {
     constructor(element) {
         this.element = element;
-    }
 
-    moveRight() {
-        let = controlRight = document.querySelector(".Carousel__arrow-right");console.log(constrolRight);
-        controlRight.addEventListener("click", () => {
-            console.log("click");
-        });
-    }
-
-    moveLeft() {
-        let controlLeft = element.querySelector(".Carousel__arrow-left");this.controlLeft.addEventListener("click", () => {
-            console.log("click");
-        });
-    }
-}
-
-class Slides {
-    constructor(element) {
-        this.element = element;
-        this.slides = element.querySelectorAll(".Carousel__items");
+        this.slides = element.querySelectorAll(".Carousel__item");
         this.slides = Array.from(this.slides);
-        console.log(slides);
+        this.index = 0;
+        this.active = this.slides[this.index];
+    
+
+        this.controlRight = element.querySelector(".Carousel__arrow-right");
+        this.controlRight.addEventListener("click", () => {
+            this.active.classList.remove(".Carousel__item-focused");
+            if (this.index < this.slides.length) {
+               this.active = this.slides[this.index ++];
+                console.log(this.index);
+                console.log(this.active);
+                // this.active.classList.add(".Carousel__item-focused");
+                console.log("clickR"); 
+            }
+            else {
+                this.active = this.slides[0];
+            }
+            
+        });
+
+        this.controlLeft = element.querySelector(".Carousel__arrow-left");
+        this.controlLeft.addEventListener("click", () => {
+            console.log("clickL");
+        });
     }
 }
+
+
 
 let carousels = document.querySelectorAll(".Carousel");
 carousels = Array.from(carousels).map(carousel => new Carousel(carousel));

52b981d59b7cf1046cb2c40896779baf91143138
Lisa Cee <three_nineteen@yahoo.com>
2018-03-17 12:04:03 -0700

    carousel wip, undefined outside of slides.length

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -11,8 +11,8 @@ class Carousel {
         this.controlRight = element.querySelector(".Carousel__arrow-right");
         this.controlRight.addEventListener("click", () => {
             this.active.classList.remove("Carousel__item-focused");
-            if (this.index < this.slides.length) {
-               this.active = this.slides[this.index ++];
+            if (this.index < this.slides.length - 1) {
+               this.active = this.slides[++this.index];
                 console.log(this.index);
                 console.log(this.active);
                 this.active.classList.add("Carousel__item-focused");
@@ -25,7 +25,16 @@ class Carousel {
 
         this.controlLeft = element.querySelector(".Carousel__arrow-left");
         this.controlLeft.addEventListener("click", () => {
-            console.log("clickL");
+            this.active.classList.remove("Carousel__item-focused");
+            if (this.index < this.slides.length) {
+               this.active = this.slides[--this.index];
+                console.log(this.index);
+                console.log(this.active);
+                
+            }
+            else {
+                this.active = this.slides[this.index = 0];this.active.classList.add("Carousel__item-focused");
+            }
         });
     }
 }

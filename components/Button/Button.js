c2cfaab6a6211337f1adb2df972a4586d9c4eddd
ubuntu <irishseanvaldivia@gmail.com>
2018-03-18 15:09:40 -0500

    Well this code made all the damn buttons dissappear...

--- components/Button/Button.js
+++ components/Button/Button.js
@@ -1,7 +1,12 @@
 const buttons = document.querySelectorAll('Button');
 
-
+const vanish = () => {
+  buttons.forEach(function(button) {
+    button.classList.toggle('vanish');
+  })
+}
 
 buttons.forEach(function(button) {
   button.addEventListener('click', vanish);
 });
+

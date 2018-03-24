7545bc49e29d0255a804b923affef40ce8264c9a
Lisa Cee <three_nineteen@yahoo.com>
2018-03-24 11:35:59 -0700

    got carousel working

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,43 +1,34 @@
 class Carousel {
     constructor(element) {
         this.element = element;
-
-        this.slides = element.querySelectorAll(".Carousel__item");
-        this.slides = Array.from(this.slides);
-        this.index = 0;
-        this.active = this.slides[this.index];
-    
-
-        this.controlRight = element.querySelector(".Carousel__arrow-right");
-        this.controlRight.addEventListener("click", () => {
-            this.active.classList.remove("Carousel__item-focused");
-            if (this.index < this.slides.length - 1) {
-               this.active = this.slides[++this.index];
-                console.log(this.index);
-                console.log(this.active);
-                this.active.classList.add("Carousel__item-focused");
-            }
-            else {
-                this.active = this.slides[this.index = 0];
-            }
-            
-        });
-
-        this.controlLeft = element.querySelector(".Carousel__arrow-left");
-        this.controlLeft.addEventListener("click", () => {
-            this.active.classList.remove("Carousel__item-focused");
-            if (this.index < this.slides.length) {
-               this.active = this.slides[--this.index];
-                console.log(this.index);
-                console.log(this.active);
-                
-            }
-            else {
-                this.active = this.slides[this.index = 0];this.active.classList.add("Carousel__item-focused");
-            }
-        });
+        this.slides = document.querySelectorAll(".Carousel__item");
+        // this.slides = Array.from(this.slides);
+        this.controlRight = document.querySelector(".Carousel__arrow-right");this.controlLeft = document.querySelector(".Carousel__arrow-left");this.index = 0;
+        this.controlRight.addEventListener('click', () => { this.clickRight() });
+        this.controlLeft.addEventListener('click', () => { this.clickLeft() });
     }
-}
+
+        clickRight() {
+            if (this.index + 1 === this.slides.length) {
+                return;
+            }
+            const index = this.index;
+            this.slides[index].classList.remove("Carousel__item-focused");
+            this.slides[index + 1].classList.add("Carousel__item-focused");
+            this.index++;
+        };
+
+        
+        clickLeft() {
+            if (this.index === 0) {
+                return;
+            }
+            const index = this.index;
+            this.slides[index].classList.remove("Carousel__item-focused");
+            this.slides[index - 1].classList.add("Carousel__item-focused");
+            this.index--;
+        };
+    }
 
 
 

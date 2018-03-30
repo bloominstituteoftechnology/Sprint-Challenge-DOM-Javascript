fd841dfc51ae8b35c47e60ecd40b4c6185bf9844
Jonathan Greene <greeneselectronics@gmail.com>
2018-03-30 16:46:24 -0600

    Just need to get it so content changes now

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -8,6 +8,9 @@ class Carousel {
         this.items = Array.from(this.items).map((item) => {
             return new CarouselItems(item, this);
         })
+
+        this.activeItem = this.items[0];
+        this.init();
        
         //event listener for clicking
          element.addEventListener('click' , (test) => {
@@ -36,24 +39,57 @@ class Carousel {
     //left arrow 
     moveLeft() {
         console.log("Left arrow was pushed");
+        this.element.classList.add("Carousel__arrow-left");
     }
 
     //right arrow
     moveRight() {
         console.log("Right arrow was pushed");
-    } 
+        this.element.classList.add("Carousel__arrow-right");
+    }
+    
+    init() {
+        // select the first link and tab upon ititialization
+        this.activeItem.select();
+      }
+    
+      updateActive(newActive) {
+        // deselect the old active link
+        this.activeItem.deselect()
+        // assign the new active link
+        this.activeItem = newActive;
+        this.activeItem.select();
+      }
+    
+      getItem(data) {
+        // use the tab item classname and the data attribute to select the proper tab
+        return this.element.querySelector(`.Carousel__item[data-number="${data}"]`);
+        
+      }
 
 }
 
 class CarouselItems {
-    constructor(element) {
+    constructor(element, parent) {
         this.element = element; 
         this.item = element.querySelectorAll(".Carousel__item");
+        this.items = parent;// attach parent to object
+        this.carouselItem = this.items.getItem(this.element.dataset.number);
+        this.carouselItem = new CarouselItem(this.carouselItem)
         this.item = Array.from(this.item).map((item) => {
             return new CarouselItem(item,this);
         })
-        console.log("CarouselItems is being called. ")
+        console.log("CarouselItems is being called.")
+        this.select();
     }
+
+    select() {
+        // select this link
+
+        console.log("Select is being ran!");
+        //this.element.classList.add("Carousel__item-focused");
+        // select the associated tab
+     }
 }
 
 
@@ -61,8 +97,11 @@ class CarouselItems {
 class CarouselItem {
     constructor(element) {
         this.element = element;
-        console.log("CarouselItem is being called. ") // this gets called once for each time = 3
+        console.log("CarouselItem is being called.") // this gets called once for each time = 3
+        console.log(element);
     }
+
+
 }
 
 let carousels = document.querySelectorAll(".Carousel");

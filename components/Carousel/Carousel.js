a1023df5869ff30e9c09a44578d1793a1a7bdf06
Jonathan Greene <greeneselectronics@gmail.com>
2018-03-30 14:43:00 -0600

    Added in the Carousel and CarouselItems classes

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,5 +1,20 @@
-class Carousel {
 
+class Carousel {
+    constructor(element) {
+        this.element = element;
+        this.items = element.querySelectorAll(".Carousel__items");
+        this.items = Array.from(this.items).map((item) => {
+            return new CarouselItems(item, this);
+        })
+    }
+
+}
+
+class CarouselItems {
+    constructor(element) {
+        this.element = element; 
+        console.log("CarouselItems is being called. ")
+    }
 }
 
 let carousels = document.querySelectorAll(".Carousel");

01ad1f14b7667e8067e2a7d60f95589f650ffa80
Kyle Kauzlarich <kauzlarich.kyle@gmail.com>
2018-03-30 11:24:17 -0700

    funcinally identical to a carousel

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -17,12 +17,68 @@ to unhide next and wich one to hid each if should hide itself
 and unhid the next slide */
 
 
-let slideBoxes = document.getElementsByClassName('Box');
+let slideBoxes = document.getElementsByClassName('Carousel__item');
+let left = document.getElementsByClassName('Carousel__arrow-left')[0];
+let right = document.getElementsByClassName('Carousel__arrow-right')[0];
 
+const slideOperatorRight = function() {
+    //start with the ifs and toggle Carousel__item-focused
 
+       if (slideBoxes[0].classList.contains('Carousel__item-focused')) {
+           slideBoxes[0].classList.toggle('Carousel__item-focused');
+           slideBoxes[1].classList.toggle('Carousel__item-focused');
+           // console.log('slide 2 should be up now');
+           return;
+       }
+       if (slideBoxes[1].classList.contains('Carousel__item-focused')) {
+           slideBoxes[1].classList.toggle('Carousel__item-focused');
+           slideBoxes[2].classList.toggle('Carousel__item-focused');
+           // console.log('slide 3 should be up now');
+           return;
+       }
+       if (slideBoxes[2].classList.contains('Carousel__item-focused')) {
+           slideBoxes[2].classList.toggle('Carousel__item-focused');
+           slideBoxes[0].classList.toggle('Carousel__item-focused');
+           // console.log('slide 1 should be up now');
+           return;
+       }
 
-
-for (let i = 2; i < slideBoxes.length; i++) {
-    //buttons[i].addEventListener('click', hider);
-    console.log(slideBoxes[i]);
 }
+
+const slideOperatorLeft = function() {
+    //start with the ifs and toggle Carousel__item-focused
+
+       if (slideBoxes[0].classList.contains('Carousel__item-focused')) {
+           slideBoxes[0].classList.toggle('Carousel__item-focused');
+           slideBoxes[2].classList.toggle('Carousel__item-focused');
+           // console.log('slide 3 should be up now');
+           return;
+       }
+       if (slideBoxes[2].classList.contains('Carousel__item-focused')) {
+           slideBoxes[2].classList.toggle('Carousel__item-focused');
+           slideBoxes[1].classList.toggle('Carousel__item-focused');
+           // console.log('slide 2 should be up now');
+           return;
+       }
+       if (slideBoxes[1].classList.contains('Carousel__item-focused')) {
+           slideBoxes[1].classList.toggle('Carousel__item-focused');
+           slideBoxes[0].classList.toggle('Carousel__item-focused');
+           // console.log('slide 1 should be up now');
+           return;
+       }
+
+}
+
+
+// console.log(left);
+// console.log(right);
+
+
+// for (let i = 0; i < slideBoxes.length; i++) {
+//     //buttons[i].addEventListener('click', hider);
+//     console.log(slideBoxes[i]);
+// }
+
+
+left.addEventListener('click', slideOperatorLeft);
+right.addEventListener('click', slideOperatorRight);

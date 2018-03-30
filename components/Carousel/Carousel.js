e20fbd1f814387016afc748c21fa42fbe8f14196
EricGomez101 <eric_gomez_1@hotmail.com>
2018-03-30 12:52:11 -0700

    finished what i could so far

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,6 +1,19 @@
 class Carousel {
-  
+  constructor(element) {
+    this.element = element;
+    this.buttonLeft = this.element.children[0];
+    this.buttonRight = this.element.children[2];
+    this.itemList = document.querySelectorAll('.Carousel__item');
+    this.activeItem = this.itemList[i];
+    this.element.addEventListener('click', (event) => {
+      if (event.target === this.buttonRight) {
+        this.activeItem.classList.remove('Carousel__item-focused');
+        i += 1;
+        
+      }
+    });
+  }
 }
-
+let i = 0;
 let carousels = document.querySelectorAll(".Carousel");
 carousels = Array.from(carousels).map(carousel => new Carousel(carousel));

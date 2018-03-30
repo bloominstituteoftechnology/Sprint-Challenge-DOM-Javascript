46e23f67848d821f1e51bd38e980e91dd4784f9d
Iqra Javed <iqrajaved@Iqras-MacBook-Air.local>
2018-03-30 11:45:24 -0500

    tried debugging using for loop, and ES6 syntax, but bug still remains

--- components/Button/Button.js
+++ components/Button/Button.js
@@ -1,7 +1,38 @@
 let buttons = document.querySelectorAll('.Button'); //r returns NodeList of 3 elements containing Button class
 
-buttons.forEach((button) => {
-    button.addEventListener('click', () => {
-        button.classList.toggle('Button--hide');
-    })
-})
+
+//################## ES5 Syntax #################
+// buttons = Array.from(buttons).map((button) => {
+    
+//     button.addEventListener('click', () => {
+        
+//         button.classList.toggle('Button--hide');
+       
+//     })
+//     console.log(buttons)
+// })
+
+//################## USING FOR LOOP #################
+//     for (let i = 0; i < buttons.length; i++) {
+    
+//     buttons[i].addEventListener('click', (event) => {
+         
+//         event.target.classList.toggle('Button--hide');
+    
+//     })
+    
+// }
+//################## ES6 Syntax #################
+class Button {
+    constructor(element){
+      this.element = element;
+      this.element.addEventListener('click', (event) => {
+        this.element.classList.toggle('Button--hide');
+      });
+    }
+  }
+  
+ buttons = Array.from(buttons).map(item => {
+    new Button(item);
+  });
+

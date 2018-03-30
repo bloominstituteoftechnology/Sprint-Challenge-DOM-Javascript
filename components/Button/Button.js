bb4d263620bba6559ba9afaa47d95eaa8f7fcd69
jeffrey <jeffreyneilflynn@gmail.com>
2018-03-30 13:01:54 -0400

    basically finished the bottons

--- components/Button/Button.js
+++ components/Button/Button.js
@@ -3,18 +3,32 @@ class Button {
 		this.element = element;
 		this.buttons = document.querySelectorAll('.Button__actual');
 		this.buttons = Array.from(this.buttons).map(item => {
-			return new Disguise(item);
+			return new Disguise(item, this);
 		}); //add event listener to each .Button__actual
+	}
+
+	dataNumber(input) {
+		this.dataAttribute = input.dataset.num;
+		return this.dataAttribute;
 	}
 }
 
 class Disguise {
-	constructor(element) {
+	constructor(element, parent) {
 		this.element = element;
+		this.parent = parent;
 		this.body = document.body;
+		this.dataNum = this.parent.dataNumber(this.element);
 		this.element.addEventListener('click', () => {
-			this.invert(); // inverts page colors
+			this.reappear(); // will display any active (hidden) buttons
 			this.disguiseButton(); // apply a class that hides the button
+			this.invert(); // inverts page colors
+		});
+	}
+
+	reappear() {
+		this.parent.buttons.forEach(button => {
+			button.element.classList.remove('Button__actual--phantom');
 		});
 	}
 
@@ -23,7 +37,8 @@ class Disguise {
 	}
 
 	invert() {
-		this.body.classList.toggle('Body--invert');
+		if (this.dataNum === "2") this.body.classList.toggle('Body--invert');
+		else this.body.classList.remove('Body--invert');
 	}
 }
 

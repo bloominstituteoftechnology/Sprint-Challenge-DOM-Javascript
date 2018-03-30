6195f143e9f2b961a221817a0dd85fa22d76f220
Justin Hong <jhong2009@gmail.com>
2018-03-30 11:15:57 -0700

    added console.logs, rearranged constructor, added updateActive method

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -24,37 +24,53 @@ class Carousel {
 		this.element = element;
 		// store right arrow
 		this.rightArrow = document.querySelector('.Carousel__arrow-right');
+		// console.log(this.rightArrow);
 		// store left arrow
 		this.leftArrow = document.querySelector('.Carousel__arrow-left');
-		// add an event listener to the right arrow
-		document.querySelector('.Carousel__arrow-left').addEventListener('click', this.previousSlide());
-		// add an event listener to the left arrow
-		document.querySelector('.Carousel__arrow-right').addEventListener('click', this.nextSlide());
+		// console.log(this.leftArrow);
 		// store reference to all carousel items
 		this.items = document.querySelectorAll('.Carousel__item');
 		// stores all instances of CarouselItem
 		this.items = Array.from(this.items).map((item) => new CarouselItem(item));
+		// console.log(this.items);
+		// console.log(this.items.length);
 		// reference the first carousel item
 		this.activeIndex = 0;
 		// show the first slide
 		this.init();
+		// add an event listener to the right arrow
+		document.querySelector('.Carousel__arrow-left').addEventListener('click', this.previousSlide());
+		// add an event listener to the left arrow
+		document.querySelector('.Carousel__arrow-right').addEventListener('click', this.nextSlide);
 	}
 	// Prototype Methods
 	// show the first active item on page load
 	init() {
 		this.items[this.activeIndex].show();
 	}
+
+	updateActive(newActive) {
+    	// deselect the old active slide item
+    	// assign the new active slide index
+    	this.items[this.activeIndex].hide();
+    	this.items[this.activeIndex] = newActive;
+ 	 }
+
 	// hides the current slide and displays the previous slide
 	previousSlide() {
-		// this.items[this.activeIndex].remove('Carousel__item-focused');
+		// console.log(this.items[this.activeIndex]);
+		this.items[this.activeIndex].hide();
 		// decrement index to go to a previous slide
-		this.activeIndex--;
-		if (this.activeIndex < 0) {
-			this.activeIndex = this.items.length;
-		} else if (this.activeIndex > this.items.length) {
-			this.activeindex = this.items[0];
-		}
-		this.items[this.activeIndex].show('Carousel__item-focused');
+		// this.activeIndex--;
+		// // console.log(this.activeIndex);
+		// if (this.activeIndex < 0) {
+		// 	this.activeIndex = this.items.length - 1;
+		// 	// console.log(this.activeIndex);
+		// } else if (this.activeIndex > this.items.length) {
+		// 	this.activeindex = 0;
+		// 	// console.log(this.activeIndex);
+		// }
+		// this.items[this.activeIndex].show('Carousel__item-focused');
 	}
 	// hides the current slide and displays the next slide
 	nextSlide() {
@@ -64,3 +80,4 @@ class Carousel {
 
 let carousels = document.querySelectorAll(".Carousel");
 carousels = Array.from(carousels).map(carousel => new Carousel(carousel));
+// console.log(carousels);

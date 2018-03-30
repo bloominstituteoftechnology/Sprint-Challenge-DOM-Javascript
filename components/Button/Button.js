65494284aa7f8aa7c72b471e71de2e8504f483c1
Axhon <axhonruiz@gmail.com>
2018-03-30 07:13:33 -1000

    styled button

--- components/Button/Button.js
+++ components/Button/Button.js
@@ -1,4 +1,4 @@
-let buttons = [...document.querySelectorAll('.Button')]
+const buttons = [...document.querySelectorAll('.Button')]
 
 const makeButtDissapear = e => (e.target.style.display = 'none')
 buttons.map(butt => butt.addEventListener('click', makeButtDissapear))

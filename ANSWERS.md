635f737213fdbf1b825434df56e4eb9d6046801d
jeffrey <jeffreyneilflynn@gmail.com>
2018-03-30 11:19:00 -0400

    buttons have base functionality

---
+++ ANSWERS.md
@@ -0,0 +1,19 @@
+What is the DOM?
+
+the document object model is a tree-like diagram representing the html elements on a page. it extends developers the ability to manipulate elements.
+
+
+
+What is an event?
+
+
+
+
+
+What is an event listener?
+
+
+
+
+
+What is a Constructor?
---
+++ components/Button/Button.js
@@ -0,0 +1,25 @@
+class Button {
+	constructor(element) {
+		this.element = element;
+		this.buttons = document.querySelectorAll('.Button__actual');
+		this.buttons = Array.from(this.buttons).map(item => {
+			return new Handler(item, this);
+		}); //add event listener to each .Button__actual
+	}
+}
+
+class Handler {
+	constructor(element, parent) {
+		this.element = element;
+		this.element.addEventListener('click', () => {
+			this.hideButton(); // apply a class that hides the button
+		});
+	}
+
+	hideButton() {
+		this.element.classList.toggle('Button__actual--hide');
+	}
+}
+
+const mainButtonClass = document.querySelector('.Button');
+new Button(mainButtonClass);

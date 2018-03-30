d8f14dceef594cab17288cbcc9da5776fb8004af
Axhon <axhonruiz@gmail.com>
2018-03-30 07:04:17 -1000

    buttons dissapear on click

---
+++ components/Button/Button.js
@@ -0,0 +1,4 @@
+let buttons = [...document.querySelectorAll('.Button')]
+
+const makeButtDissapear = e => (e.target.style.display = 'none')
+buttons.map(butt => butt.addEventListener('click', makeButtDissapear))

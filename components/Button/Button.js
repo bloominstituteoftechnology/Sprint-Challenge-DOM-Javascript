fd1f7ec9dee705a7857be003b9097c0710d2a6a9
Justin Hong <jhong2009@gmail.com>
2018-03-30 08:31:00 -0700

    added more comments to html page and set up Button.js

---
+++ components/Button/Button.js
@@ -0,0 +1,6 @@
+alert('Hello');
+// access the 3 buttons
+let buttons = document.querySelectorAll('.Button');
+console.log(buttons);
+// iterate over the collection of buttons
+// give each button an event listener for a 'click' event'

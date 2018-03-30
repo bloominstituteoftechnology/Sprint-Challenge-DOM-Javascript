7acc53298f1d95149c38bee845762ec5ab13de67
Justin Hong <jhong2009@gmail.com>
2018-03-30 08:37:05 -0700

    set up Button class

--- components/Button/Button.js
+++ components/Button/Button.js
@@ -1,6 +1,18 @@
-alert('Hello');
+// Button Component
+class Button {
+	constructor() {
+		// store each button itself
+		// add an event listener to each button
+	}
+	// Prototype methods
+	// callback for when a button's event listener is triggered
+}
+
 // access the 3 buttons
 let buttons = document.querySelectorAll('.Button');
-console.log(buttons);
+// console.log(buttons);
+
 // iterate over the collection of buttons
-// give each button an event listener for a 'click' event'
+// create an instance of class Button for each button
+buttons = Array.from(buttons).map((btn) => new Button(btn));
+console.log(buttons);

65642872f304e6b953f0ee72ef71dd5f39a50a97
Paul Fang <paulfang@pauls-mbp.home>
2018-03-30 11:02:28 -0700

    more js fixes

--- components/Button/Button.js
+++ components/Button/Button.js
@@ -3,12 +3,13 @@ class Button {
         this.element = element;
         this.element.addEventlistener('click', () => {
             this.hideButton();
-        })
+        });
 }
 
     hideButton() {
         let element = this.element;
-        element.style.display = "none";    }
-
+        element.style.display = "none";    
+    }
+}
 let buttons = document.querySelectorAll(".Button");
 buttons = Array.from(buttons).map(button => new Button(button));

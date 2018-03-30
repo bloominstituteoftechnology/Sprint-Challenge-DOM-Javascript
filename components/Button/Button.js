60faa6f72d9affbd0c486157c583fbcc916998a4
Devon Bacon <devon.bacon@gmail.com>
2018-03-30 08:00:35 -0700

    Added functionality to buttons

---
+++ components/Button/Button.js
@@ -0,0 +1,10 @@
+// Reference DOM elements
+const buttons = document.querySelectorAll('.Button');
+
+// Add event listener for each button
+for(let i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener('click', function() {
+    this.parentNode.removeChild(this);
+  })
+}
+

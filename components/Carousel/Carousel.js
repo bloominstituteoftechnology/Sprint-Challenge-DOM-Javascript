17629c953354c52192e1a30dbdb881e39aff0baa
Tylar Pierson <tylarpiersonbusiness@gmail.com>
2018-03-30 10:19:17 -0700

    working on carousel js

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -4,6 +4,23 @@ class Carousel {
         this.left = this.element.querySelector('.Carousel__arrow-left');
         this.right = this.element.querySelector('.Carousel__arrow-right');
         this.items = this.element.querySelectorAll('.Carousel__items');
+        this.counter = 0;
+        this.amount = this.items.length;
+        this.current = this.element.querySelector('.Carousel__item-focused');
+
+        this.left.addEventListener('click', () => {
+            this.move(-1);
+        });
+        this.right.addEventListener('click', () => {
+            this.move(1);
+        });
+    }
+
+    move(direction) {
+        this.current.classList.remove(this.current);
+        this.counter = 
+        this.current = this.items[this.counter];
+        this.current.classList.add('.Carousel__item-focused');
     }
 }
 

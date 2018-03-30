559ba1fed724f979e75699e8e09cdf1c96a5745f
Matthias Bakken <matthias.bakken@gmail.com>
2018-03-30 10:24:20 -0700

    finished project and just completed sudo code for button.js

--- components/Button/Button.js
+++ components/Button/Button.js
@@ -6,12 +6,14 @@ class Button {
     // each button will be passed into the constructor as the element
     constructor(element) {
         this.element = element;
+        // adding an event listener to each button for when the button is clicked
         this.element.addEventListener('click', () => {this.buttonClick()});
     }
 
     buttonClick() {
+        // when button is clicked this will toggle the Button--Clicked css class on the button
         this.element.classList.toggle('Button--Clicked');
     }
 }
-
+// this creates an instance of each button in buttons and passes them into the Button class
 buttons = Array.from(buttons).map( button => new Button(button));

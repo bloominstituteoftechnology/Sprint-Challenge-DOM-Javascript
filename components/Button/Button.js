a19cc0e6a224cc983d6bcbecfad816c8c9b23991
imsdhk <jappleleaf@gmail.com>
2018-03-30 11:20:25 -0400

    created a hideMe method to the class and created instances of the class using forEach

--- components/Button/Button.js
+++ components/Button/Button.js
@@ -4,10 +4,15 @@ class Button {
     constructor(element){
         this.element = element;
         this.element.addEventListener('click', () => {
-            this.hide();
+            this.hideMe();
         })
+    }
+
+    hideMe(){
+        this.element.classList.add('Button--hide'); 
     }
 
 }
 
 let buttons = Array.from(document.querySelectorAll(".Button"));
+buttons.forEach(el => new Button(el)); 

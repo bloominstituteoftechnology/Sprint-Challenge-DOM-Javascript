41ce7adadbe20c462fb683bd15a3e4ff324a77e8
Henry K <mykim1891@hotmail.com>
2018-03-30 12:20:56 -0700

    Disappearing button done; working on stretch

---
+++ ANSWERS.md
@@ -0,0 +1,7 @@
+1. DOM is Document Object Model, where JavaScript can dynamically change the elements and attributes of html and CSS without having to change the actual codes of both html and CSS.
+
+2. An event occurs is when JavaScript reacts to user activity (such as click or hover) on particular object of html.
+
+3. An event listener is an object that handles the event prompted by user
+
+4. A constructor is a special menthod (function) that is called in order to create and initialize an object instance within a class.
--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,6 +1,55 @@
-class Carousel {
+class Carouselbox {
+    constructor(element, parent) {
+        this.element = element;
+        // console.log(this.element);
+    }
+
+    feature() {
+        this.element.classList.add('Carousel__item-focused') //on switch
+    }
+
+    hide()  {
+        this.element.classList.remove('Carousel__item-focused') //off switch
+    }
+
 
 }
 
-let carousels = document.querySelectorAll(".Carousel");
-carousels = Array.from(carousels).map(carousel => new Carousel(carousel));
+class Carousel {
+    constructor(element) {
+        this.element = element;
+        this.carouselBox = element.querySelectorAll('.Carousel__item');
+        this.carouselBox = Array.from(this.carouselBox).map((item) => {
+            return new Carouselbox(item, this)
+        });
+        this.activeCarousel = this.carouselBox[0];
+        this.updateCarousel_left();
+        this.updateCarousel_right();
+    }
+    
+    updateCarousel_left() {
+        this.leftArrow = this.element.querySelector('.Carousel__arrow-left');
+
+        this.leftArrow.addEventListener('click', () => {
+            // click left, chnage by -1 based on data.caroItem
+            // this.Carouselbox.feature();
+            console.log(`clicking left`);
+
+        });
+    }
+
+    updateCarousel_right() {
+        this.rightArrow = this.element.querySelector('.Carousel__arrow-right');
+
+        this.rightArrow.addEventListener('click', () => {
+            // click right, chnage by +1 based on data.caroItem
+            // this.Carouselbox.feature();
+            console.log(`clicking right`);
+
+        });
+    }
+
+}
+
+let carousel = document.querySelectorAll(".Carousel");
+carousel = Array.from(carousel).map(carousel => new Carousel(carousel));

3d4c084c26ec9846c8f259d6277f5064a667030e
nickbrenn <nbren@tutanota.com>
2018-03-30 12:03:42 -0400

    Got the buttons to hide when clicked and styled them slightly

--- components/Button/Button.js
+++ components/Button/Button.js
@@ -1,20 +1,12 @@
 class Button {
     constructor(element){
-        console.log('hi');
         this.element = element;
-        console.log(this.element);
-       // this.button = document.querySelectorAll('.Button');
-        this.element.addEventListener('click', this.hideButton);
+        this.element.addEventListener('click', () => { this.hideButton()});
     }
     hideButton(){
-        console.log('hidey');
-        console.log(this.element);
-        this.element.classList.add('Button--hide');
+        this.element.classList.toggle('Button--hide');
     }
 }
 
 let buttons = document.querySelectorAll('.Button');
 buttons =  Array.from(buttons).map(buttons => new Button(buttons));
-//buttons =  new Button(buttons);
-
-console.log(buttons);

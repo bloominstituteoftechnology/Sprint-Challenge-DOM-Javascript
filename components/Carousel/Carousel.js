57811c13d5740986186b0cecbd6402c5c96b7834
Justin Hong <jhong2009@gmail.com>
2018-03-30 09:55:01 -0700

    set up Carousel class constructor

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -11,8 +11,17 @@ class CarouselItem {
 // contains information about each carousel item
 // controls the carousel right and left arrows 
 class Carousel {
-	constructor() {
-
+	constructor(element) {
+		// store the carousel div
+		this.element = element;
+		// add an event listener to the right arrow
+		document.querySelector('.Carousel__arrow-left').addEventListener('click', () => nextSlide());
+		// add an event listener to the left arrow
+		document.querySelector('.Carousel__arrow-right').addEventListener('click', () => nextSlide());
+		// store reference to all carousel items
+		this.items = document.querySelectorAll('.Carousel__item');
+		// stores all instances of CarouselItem
+		this.items = Array.from(this.items).map((item) => new CarouselItem(item));
 	}
 	// Prototype Methods
 

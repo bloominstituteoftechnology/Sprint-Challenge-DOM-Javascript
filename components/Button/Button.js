d303dcec849ebe905a4784b3f3f308d8470c7691
Mark Wong <fmarkwong@gmail.com>
2018-03-30 09:03:12 -0700

    works

---
+++ components/Button/Button.js
@@ -0,0 +1,13 @@
+
+class Button {
+  constructor(button) {
+    this.button = button;
+    this.button.addEventListener('click', () => this.dissapear());
+  }
+
+  dissapear() {
+    this.button.classList.add('dissapear');
+  }
+}
+
+let buttons = Array.from(document.querySelectorAll('.Button')).map(button => new Button(button));

a6c1dcbec75fc6c8694a4127877265b2482eba50
jeffrey <jeffreyneilflynn@gmail.com>
2018-03-30 12:13:27 -0400

    added some cool features to the buttons

--- components/Button/Button.js
+++ components/Button/Button.js
@@ -3,24 +3,35 @@ class Button {
 		this.element = element;
 		this.buttons = document.querySelectorAll('.Button__actual');
 		this.buttons = Array.from(this.buttons).map(item => {
-			return new Handler(item, this);
+			return new Disguise(item);
 		}); //add event listener to each .Button__actual
 	}
 }
 
-class Handler {
-	constructor(element, parent) {
+class Disguise {
+	constructor(element) {
 		this.element = element;
+		this.body = document.body;
 		this.element.addEventListener('click', () => {
-			this.hideButton(); // apply a class that hides the button
+			this.invert(); // inverts page colors
+			this.disguiseButton(); // apply a class that hides the button
 		});
 	}
 
-	hideButton() {
-		// this.element.classList.toggle('Button__actual--hide');
+	disguiseButton() {
 		this.element.classList.toggle('Button__actual--phantom');
 	}
+
+	invert() {
+		this.body.classList.toggle('Body--invert');
+	}
 }
+
+// class Invert extends Disguise {
+// 	constructor(element) {
+// 		this.element = element;
+// 	}
+// }
 
 const mainButtonClass = document.querySelector('.Button');
 new Button(mainButtonClass);

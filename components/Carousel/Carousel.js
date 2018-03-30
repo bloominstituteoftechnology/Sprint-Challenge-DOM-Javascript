25655ec87686279610b3d540a59d7b601e42bf8e
nickbrenn <nbren@tutanota.com>
2018-03-30 12:23:42 -0400

    Got carousel arrows to respond

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,6 +1,20 @@
 class Carousel {
+    constructor(element){
+        this.element = element;
+        this.leftArrow = element.querySelector('.Carousel__arrow-left');
+        this.rightArrow = element.querySelector('.Carousel__arrow-right');
+        this.leftArrow.addEventListener('click', () => {this.leftMove()});
+        this.rightArrow.addEventListener('click', () => {this.rightMove()});
 
+    }
+    leftMove(){
+        console.log('left');
+    }
+    rightMove(){
+        console.log('right');
+    }
 }
 
 let carousels = document.querySelectorAll(".Carousel");
 carousels = Array.from(carousels).map(carousel => new Carousel(carousel));
+console.log(carousels);

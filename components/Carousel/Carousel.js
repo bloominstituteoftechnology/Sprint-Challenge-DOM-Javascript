a7c09d6358714e7ada05fa7cd92b9f69ea7282ed
Devon Bacon <devon.bacon@gmail.com>
2018-03-30 10:58:34 -0700

    Completed carousel items class

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,9 +1,11 @@
 class Carousel {
-  constructor(element) {
+  constructor(element, items) {
+    // Carousel and each carousel item in array format
     this.element = element;
+    this.items = Array.from(items).map(item => new CarouselItem(item));
+    // Arrow elements and listeners
     this.leftArrow = element.children[0];
     this.rightArrow = element.children[2];
-    // Arrow event listeners
     this.leftArrow.addEventListener('click', this.clickLeft);
     this.rightArrow.addEventListener('click', this.clickRight);
   }
@@ -22,20 +24,37 @@ class CarouselItem {
     this.element = element;
     this.parent = element.parentNode;
 
-    console.log(this.parent);
-    this.element.addEventListener('click', this.moveLeft);
+    this.element.addEventListener('click', () => {
+      this.moveLeft();
+    });
   }
 
-  moveRight() { // Move out and to the right
-
-  }
-
-  moveLeft() { // Move out and to the left
-    console.log(this);
+  moveRight() { // Move out and to the right and fade
     const ownWidth = this.getOwnWidth();
     const parentWidth = this.getParentWidth();
+    // Allow for 2 second transitions
+    this.element.classList.add('Carousel__item-transition');
+    this.element.style.marginRight = `-${Number(parentWidth.slice(0, -2)) * 2}px`;
+    this.fadeOut();
+    // Wait until animation is done then remove from screen
+    setTimeout(() => {
+      this.element.className = 'Carousel__item';
+      this.reset();
+    }, 2000);
+  }
 
-    console.log(ownWidth, parentWidth);
+  moveLeft() { // Move out and to the left and fade
+    const ownWidth = this.getOwnWidth();
+    const parentWidth = this.getParentWidth();
+    // Allow for 2 second transitions
+    this.element.classList.add('Carousel__item-transition');
+    this.element.style.marginLeft = `-${Number(parentWidth.slice(0, -2)) * 2}px`;
+    this.fadeOut();
+    // Wait until animation is done then remove from screen
+    setTimeout(() => {
+      this.element.className = 'Carousel__item';
+      this.reset();
+    }, 2000);
   }
 
   fadeIn() { // Set opacity to 1
@@ -43,36 +62,45 @@ class CarouselItem {
   }
 
   fadeOut() { // Set opacity of 0
-    this.style.opacity = 0;
+    this.element.style.opacity = 0;
   }
 
-  center() { // Bring to center
-    this.style.margin = 0;
+  reset() { // Reset modified styles
+    this.element.style.margin = 'auto';
+    this.element.style.opacity = 1;
+    this.width = 'auto';
   }
 
   getOwnWidth() { // Detect own width to prevent resizing
-    var computedStyle = window.getComputedStyle(this);
+    var computedStyle = window.getComputedStyle(this.element);
     return computedStyle.getPropertyValue('width');
   }
 
   getParentWidth() { // Detect parent with for move distance
-    var computedStyle = window.getComputedStyle(this.parentNode);
+    var computedStyle = window.getComputedStyle(this.parent);
     return computedStyle.getPropertyValue('width');
   }
 }
 
-// Entire carousel
+// DOM references
 let carousels = document.querySelectorAll(".Carousel");
-//console.log(carousels);
-carousels = Array.from(carousels).map(carousel => new Carousel(carousel));
-// Individual carousel items
 let carouselItems = document.querySelectorAll('.Carousel__item');
-//console.log(carouselItems);
-carouselItems = Array.from(carouselItems).map(item => new CarouselItem(item));
-console.log(carouselItems);
+carousels = Array.from(carousels).map(carousel => new Carousel(carousel, carouselItems));
+
+// // Entire carousel
+// let carousels = document.querySelectorAll(".Carousel");
+// //console.log(carousels);
+// carousels = Array.from(carousels).map(carousel => new Carousel(carousel));
+// // Individual carousel items
+// let carouselItems = document.querySelectorAll('.Carousel__item');
+// //console.log(carouselItems);
+// carouselItems = Array.from(carouselItems).map(item => new CarouselItem(item));
+
+
+
+
 
 // const carouselItems = document.querySelector('.Carousel__items');
-
 
 // for (let i = 0; i < carouselItems.children.length; i++) {
 //   carouselItems.children[i].addEventListener('click', function() {

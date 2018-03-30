4615b7865df1900b5e90831e78cbdce807a4fada
Tylar Pierson <tylarpiersonbusiness@gmail.com>
2018-03-30 10:47:27 -0700

    got carousel js working just figuring out how to change slides

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -4,23 +4,64 @@ class Carousel {
         this.left = this.element.querySelector('.Carousel__arrow-left');
         this.right = this.element.querySelector('.Carousel__arrow-right');
         this.items = this.element.querySelectorAll('.Carousel__items');
-        this.counter = 0;
-        this.amount = this.items.length;
+        this.amount = this.items.length - 1;
         this.current = this.element.querySelector('.Carousel__item-focused');
 
+        this.box(this.current);
+
         this.left.addEventListener('click', () => {
-            this.move(-1);
+            console.log('clicked');
+            this.moveLeft();
         });
         this.right.addEventListener('click', () => {
-            this.move(1);
+            console.log('clicked');
+            this.moveRight();
         });
     }
 
-    move(direction) {
-        this.current.classList.remove(this.current);
-        this.counter = 
-        this.current = this.items[this.counter];
-        this.current.classList.add('.Carousel__item-focused');
+    moveLeft(interval) {
+        (this.current === 0) ? this.current = this.amount : this.current -= 1;
+
+        this.stop();
+        this.box(this.current);
+
+        if (typeof interval === 'number' && (interval % 1) === 0) {
+            let context = this;
+            this.run = setTimeout(() => {
+                context.moveLeft(interval);
+            }, interval);
+        }
+    }
+    
+    moveRight(interval) {
+        (this.current === this.amount) ? this.current = 0 : this.current += 1;
+
+        this.stop();
+        this.box(this.current);
+
+        if (typeof interval === 'number' && (interval % 1) === 0) {
+            let context = this;
+            this.run = setTimeout(() => {
+                context.moveRight(interval);
+            }, interval);
+        }
+    }
+
+    stop() {
+        clearTimeout(this.run);
+    }
+
+    box(index) {
+        if (index >= 0 && index <= this.amount) {
+            this.stop();
+            for (let b = 0; b <= this.amount; b++) {
+                if (b === index) {
+                    this.box[s].style.display = "inline-block";
+                } else {
+                    this.box[s].style.display = "none";
+                }
+            }
+        }
     }
 }
 

2b4edccfdf47e10628a993c62fb264bf962838ac
Robert Hogan <roberthogan@Roberts-MacBook-Pro.local>
2018-03-30 12:20:57 -0400

    Robert Hogan. Further progress on button, additional styles and functions added.

--- components/Button/Button.js
+++ components/Button/Button.js
@@ -1,15 +1,32 @@
+
 // create button class constructor
-class Button {
+class Button { 
     constructor(element) {
         this.element = element;
         this.element.addEventListener("click", () => {
             this.noShow();
         });
+        this.element.addEventListener("mouseenter", () => {
+            this.addBorder();
+        })
+        this.element.addEventListener("mouseleave", () => {
+            this.removeBorder();
+        })
     }
 
     //noShow function
-    noShow() {
+    noShow() { // adds display: none when user clicks on button
         this.element.style.display = "none";
+    }
+
+    //addBorder function
+    addBorder() {
+        this.element.style.border = "2px dashed red";
+    }
+
+    //removeBorder function
+    removeBorder() {
+        this.element.style.border = "3px solid black";
     }
 }
 

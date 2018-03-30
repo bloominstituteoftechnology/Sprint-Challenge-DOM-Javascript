3d4298161899b7e8239b21653f6d612b199ff668
2940cristian <2940cristian@gmail.com>
2018-03-30 10:58:42 -0700

    Could not complete carousel functionality

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,10 +1,19 @@
+class CarouselItem {
+    constructor(element) {
+        this.element = element;
+        console.log('lol')
+    }
+}
+
 class Carousel {
     constructor(element) {
         this.element = element;
-        this.items = document.querySelectorAll("Carousel__item");
+        this.items = document.querySelectorAll('.Carousel__item');
+        this.items = Array.from(this.items).map((items) => {
+            return new CarouselItem(items, this);
+        });
+        console.log('lol');
     }
-
 }
 
 let carousels = document.querySelectorAll(".Carousel");
-carousels = Array.from(carousels).map(carousel => new Carousel(carousel));

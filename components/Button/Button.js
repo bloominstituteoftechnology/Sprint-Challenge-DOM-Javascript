8c2a6167beba78d85eb56402fed313f528b27bdf
Justin Hong <jhong2009@gmail.com>
2018-03-30 08:45:07 -0700

    set up Button constructor and hide() method

--- components/Button/Button.js
+++ components/Button/Button.js
@@ -1,18 +1,25 @@
 // Button Component
 class Button {
-	constructor() {
+	constructor(element) {
 		// store each button itself
+		this.element = element;
 		// add an event listener to each button
+		this.element.addEventListener('click', this.hide);
 	}
 	// Prototype methods
 	// callback for when a button's event listener is triggered
+	hide() {
+		this.element.classList.toggle('.Button');
+	}
 }
 
 // access the 3 buttons
 let buttons = document.querySelectorAll('.Button');
-// console.log(buttons);
+console.log(buttons);
 
 // iterate over the collection of buttons
 // create an instance of class Button for each button
-buttons = Array.from(buttons).map((btn) => new Button(btn));
+buttons = Array.from(buttons).map(btn => new Button(btn));
 console.log(buttons);
+
+// tabs = Array.from(tabs).map(tabs => new Tabs(tabs));

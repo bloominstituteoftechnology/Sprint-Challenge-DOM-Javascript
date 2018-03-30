9bc110cbf21f02107da853026bfbd4186184031f
Anthony <anthony@anthonys-mbp.lan>
2018-03-30 12:38:50 -0400

    Copied dropdown functionality to 3 buttons

--- components/Dropdown/Dropdown.js
+++ components/Dropdown/Dropdown.js
@@ -1,7 +1,7 @@
 class Dropdown {
   constructor(element) {
     this.element = element;
-    this.button = this.element.querySelector(".Dropdown__button");
+    this.button = this.element.querySelector(".Dropdown__button"); 
 
     this.button.addEventListener("click", () => {
       this.toggleContent();
@@ -15,3 +15,22 @@ class Dropdown {
 
 let dropdowns = document.querySelectorAll(".Dropdown");
 dropdowns = Array.from(dropdowns).map(dropdown => new Dropdown(dropdown));
+
+class Button{
+  constructor(element) {
+    this.element = element;
+    this.button = this.element.querySelector(".Button"); 
+
+    this.button.addEventListener("click", () => {
+      this.toggleButton();
+    });
+  }
+
+  toggleButton() {
+    this.element.classList.toggle("Button--active");
+  }
+}
+
+let buttons = document.querySelectorAll(".Button");
+buttons = Array.from(buttons).map(button => new Button(button));
+

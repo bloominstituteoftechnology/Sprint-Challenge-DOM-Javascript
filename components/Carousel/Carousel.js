752daa76abcfe9516b2d7fcf50e96ecc9565ec4a
markstez05 <36493764+markstez05@users.noreply.github.com>
2018-03-30 11:53:15 -0500

    starting stretch

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,6 +1,11 @@
 class Carousel {
+    constructor(element) {
+        this.element = element;
+        this.arrow
+    }
 
 }
 
 let carousels = document.querySelectorAll(".Carousel");
 carousels = Array.from(carousels).map(carousel => new Carousel(carousel));
+

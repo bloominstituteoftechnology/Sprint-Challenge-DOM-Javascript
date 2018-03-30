a26b8ff8a4a4aa618083557c88bf0d2cd8894cd4
mgchmiel <michaelchmielniak@yahoo.com>
2018-03-30 11:00:55 -0400

    Answers

---
+++ ANSWERS.md
@@ -0,0 +1,4 @@
+1. DOM or Document Object Model is an interface for HTML documents which represents the HTML documents as an object orientated represenation of the webpage to be manipulated.
+2. Events are actions which usually cause a function or anything to be excuted.
+3. EventListener is a method which attaches an event to a specific element.
+4. A constructor is a method which creates and initiates an object within a specific class.
--- components/Button/Button.js
+++ components/Button/Button.js
@@ -2,11 +2,11 @@ class Button {
     constructor(element) {
         this.element = element;
         this.element.addEventListener("click", () => {
-            this.element.classList.add("Button--dissappear");
+            this.element.classList.add("Button--dissappear"); //click event listener activates class list in css
         });
     }
 }
 let buttons = document.querySelectorAll(".Button");
-buttons = Array.from(buttons).map(button => {
+buttons = Array.from(buttons).map(button => { //takes .Button div and turns it into an array to map into and return ne class with class list which causes each one to dissappear
     return new Button(button);
 });

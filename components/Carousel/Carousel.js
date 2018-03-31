fdea7a5286b0e1b5dca08094b9dfd2743928a0a4
jeffrey <jeffreyneilflynn@gmail.com>
2018-03-30 21:07:14 -0400

    refined my solution ... looking good

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -2,6 +2,43 @@ class Carousel {
 	constructor(element) {
 		this.element = element;
 		this.items = document.querySelectorAll('.Carousel__item');
+		this.arrow(document.querySelectorAll(".Carousel__arrow"));
+		this.index = 0;
+	}
+
+	goRight() {
+		this.items[this.index].classList.remove('Carousel__item-focused');
+		this.index += 1;
+		if (this.index === this.items.length) this.index = 0;
+		this.items[this.index].classList.add('Carousel__item-focused');
+	}
+
+	goLeft() {
+		this.items[this.index].classList.remove('Carousel__item-focused');
+		this.index -= 1;
+		if (this.index < 0) this.index = 2;
+		this.items[this.index].classList.add('Carousel__item-focused');
+	}
+
+	arrow(arrowArray) {
+		this.actualArray = Array.from(arrowArray).map(arrow => {
+			if (arrow.id === "left") arrow.addEventListener('click', () => this.goLeft());
+			else arrow.addEventListener('click', () => this.goRight());
+		});
+	}
+
+}
+
+let carousels = document.querySelectorAll(".Carousel");
+carousels = Array.from(carousels).map(carousel => new Carousel(carousel));
+
+/*************************************************************************/
+
+/*
+class Carousel {
+	constructor(element) {
+		this.element = element;
+		this.items = document.querySelectorAll('.Carousel__item');
 		this.items = Array.from(this.items);
 		this.currentlyActive = document.querySelector('.Carousel__item-focused');
 
@@ -10,8 +47,7 @@ class Carousel {
 		this.three = document.querySelector('.Carousel__item[data-num="3"]');
 
 		this.rightButton = document.querySelector('.Carousel__arrow-right');
-		// this.leftButton = document.querySelector('.Carousel__arrow-left');
-		this.leftButton = document.getElementById('left');
+		this.leftButton = document.querySelector('.Carousel__arrow-left');
 
 		this.rightButton.addEventListener('click', () => {
 			this.goRight();
@@ -79,6 +115,4 @@ class Carousel {
 	}
 
 }
-
-let carousels = document.querySelectorAll(".Carousel");
-carousels = Array.from(carousels).map(carousel => new Carousel(carousel));
+*/

51cffcff481b12a4d3bb830fb764b4bed7de145f
Devon Bacon <devon.bacon@gmail.com>
2018-03-30 19:40:29 -0700

    Added more button like functionality

--- components/Button/Button.js
+++ components/Button/Button.js
@@ -6,5 +6,8 @@ for(let i = 0; i < buttons.length; i++) {
   buttons[i].addEventListener('click', function() {
     this.parentNode.removeChild(this);
   })
+  buttons[i].addEventListener('mousedown', function() {
+    this.classList.add('Button-clicked');
+  })
 }
 

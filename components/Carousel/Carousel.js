96a21a91c4f220c7fbf152108650623705329791
Devon Bacon <devon.bacon@gmail.com>
2018-03-30 19:42:56 -0700

    I'm pretty happy with the carousel refactor

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,20 +1,59 @@
 class Carousel {
-  constructor(element, items) {
+  constructor(element) {
     // Carousel and each carousel item in array format
     this.element = element;
-    this.items = Array.from(items).map(item => new CarouselItem(item));
+    this.items = document.querySelectorAll('.Carousel__item');
+    this.items = Array.from(this.items).map(item => new CarouselItem(item));
     this.focusIndex = 0;
     // Arrow elements and listeners
     this.leftArrow = this.element.children[0];
     this.rightArrow = this.element.children[2];
     this.leftArrow.addEventListener('click', () => {
-      this.items[0].moveLeftOut();
+      this.moveLeft(this.items[this.focusIndex]);
+      console.log(this.items[this.focusIndex]);
     });
     this.rightArrow.addEventListener('click', () => {
-      this.items[0].moveRightOut();
-      console.log(this.items[this.items.length - 1]);
-      this.items[this.items.length - 1].moveRightIn();
+      this.moveRight(this.items[this.focusIndex]);
+      console.log(this.items[this.focusIndex]);
     });
+
+    this.startLeft(this.items[this.focusIndex]);
+  }
+
+  startRight(item) {
+    item.updateWidths();
+    item.element.style.marginRight = `-${item.parentWidth * 4}px`;
+  }
+
+  startLeft(item) {
+    item.updateWidths();
+    item.element.style.marginLeft = `-${item.parentWidth * 4}px`;
+  }
+
+  moveRight(item) {
+    item.toggleTransition('on');
+    item.updateWidths();
+    item.element
+    item.fadeIn();
+    // If to the left then bring to center
+    if (item.element.style.marginLeft !== '') {
+      item.element.style.marginLeft = '';
+    } else { // Otherwise move to right
+      this.startRight(item);
+    }
+  }
+
+  moveLeft(item) {
+    item.toggleTransition('on');
+    item.updateWidths();
+    item.element
+    item.fadeIn();
+    // If to the right then bring to center
+    if (item.element.style.marginRight !== '') {
+      item.element.style.marginRight = '';
+    } else { // Otherwise move to right
+      this.startLeft(item);
+    }
   }
 
   shiftLeft() { // Left arrow clicked
@@ -29,48 +68,9 @@ class Carousel {
 class CarouselItem {
   constructor(element) {
     this.element = element;
+    this.elementWidth;
     this.parent = element.parentNode;
-  }
-
-  moveRightOut() { // Move out and to the right while fading out
-    const ownWidth = this.getOwnWidth();
-    const parentWidth = this.getParentWidth();
-    // Modify margin based on parent width
-    this.element.style.marginRight = `-${Number(parentWidth.slice(0, -2)) * 2}px`;
-    this.fadeOut();
-    // Wait until animation is done then remove from screen
-    setTimeout(() => {
-      this.element.className = 'Carousel__item';
-      this.reset();
-    }, 2000);
-  }
-
-  moveRightIn() { // Move in and to the right while fading in
-    const ownWidth = this.getOwnWidth();
-    const parentWidth = this.getParentWidth();
-    // Move off screen to the left and set opacity to 0
-    this.element.style.marginLeft = `-${Number(parentWidth.slice(0, -2)) * 2}px`;
-    this.fadeOut();
-    // After other item has left screen
-    setTimeout(() => {
-      // Display element and transition to default position
-      this.element.style.margin = 'auto';
-      this.fadeIn();
-      this.element.classList.add('Carousel__item-focused');
-    }, 2000);
-  }
-
-  moveLeftOut() { // Move out and to the left while fading out
-    const ownWidth = this.getOwnWidth();
-    const parentWidth = this.getParentWidth();
-    // Modify margin based on parent width
-    this.element.style.marginLeft = `-${Number(parentWidth.slice(0, -2)) * 2}px`;
-    this.fadeOut();
-    // Wait until animation is done then remove from screen
-    setTimeout(() => {
-      this.element.className = 'Carousel__item';
-      this.reset();
-    }, 2000);
+    this.parentWidth;
   }
 
   fadeIn() { // Set opacity to 1
@@ -87,38 +87,27 @@ class CarouselItem {
     this.width = 'auto';
   }
 
-  toggleTransition(option) {
-    // To be added
+  toggleTransition(option) { // Switch 2 second transitions on/off
+    if (option === 'on' && !(this.element.classList.contains('Carousel__item-transition'))) {
+      this.element.classList.add('Carousel__item-transition');
+    } else if (option === 'off' && this.element.classList.contains('Carousel__item-transition')) {
+      this.element.classList.remove('Carousel__item-transition');
+    }
   }
 
-  getOwnWidth() { // Detect own width to prevent resizing
-    var computedStyle = window.getComputedStyle(this.element);
-    return computedStyle.getPropertyValue('width');
-  }
-
-  getParentWidth() { // Detect parent with for move distance
-    var computedStyle = window.getComputedStyle(this.parent);
-    return computedStyle.getPropertyValue('width');
+  updateWidths() { // Convert width properties into numerical values for later mutation
+    this.elementWidth = Number(window.getComputedStyle(this.element).width.slice(0, -2));
+    this.parentWidth = Number(window.getComputedStyle(this.parent).width.slice(0, -2));
   }
 }
 
 // DOM references
 let carousels = document.querySelectorAll(".Carousel");
-let carouselItems = document.querySelectorAll('.Carousel__item');
-carousels = Array.from(carousels).map(carousel => new Carousel(carousel, carouselItems));
-
-// // Entire carousel
-// let carousels = document.querySelectorAll(".Carousel");
-// //console.log(carousels);
-// carousels = Array.from(carousels).map(carousel => new Carousel(carousel));
-// // Individual carousel items
-// let carouselItems = document.querySelectorAll('.Carousel__item');
-// //console.log(carouselItems);
-// carouselItems = Array.from(carouselItems).map(item => new CarouselItem(item));
+carousels = Array.from(carousels).map(carousel => new Carousel(carousel));
 
 
 
-
+// -v-vvv-v- Prototype of my above functionality -v-vvv-v- //
 
 // const carouselItems = document.querySelector('.Carousel__items');
 

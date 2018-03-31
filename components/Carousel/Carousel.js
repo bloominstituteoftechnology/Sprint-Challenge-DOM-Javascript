de7c2b4d8fe10cd10738474c181d3bb71e97944b
JulianAlexander <julian.pineyro@gmail.com>
2018-03-31 05:56:20 +0300

    completed carousel

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,46 +1,33 @@
-class CarouselItem {
-    constructor(element, parent) {
-        this.element = element;
-        this.box = parent;
-        this.dataBox = this.box.dataCar(this.element.dataset.info);
-        this.arrowleft = new Carousel(this.carousel);
-        this.element.addEventListener('click', () => {this.dataBox})
-    }
-    
-    select() {
-        this.element.classList.add('Carousel__items--selected');
-    }
-
-    deselect() {
-        this.element.classList.remove('Carousel__items--selected');
-    }
-}
-
-
 class Carousel {
     constructor(element) {
         this.element = element;
-        this.items = element.querySelectorAll (".Carousel__items");
-        this.items = Array.from(this.items).map((item)=> {return new CarouselItem(item, this);
-        });
-        this.activeItem = this.items[0];
-        this.in();       
+        this.items = document.querySelectorAll (".Carousel__item");
+        this.arrowLeft = document.querySelector(".Carousel__arrow-left");
+        this.arrowRight = document.querySelector(".Carousel__arrow-right");
+        this.index= 0;
+        this.arrowLeft.addEventListener('click', () => {this.goLeft()})
+        this.arrowRight.addEventListener('click', () => {this.goRight()})        
     }
 
-    in() {
-        this.activeItem.select();
+    goLeft() {
+        if(this.index === 0) {
+            return;
+        }
+        const index = this.index;
+        this.items[index].classList.remove('Carousel__item-focused');
+        this.items[index - 1].classList.add('Carousel__item-focused');
+        this.index--
     }
 
-    update(newUpdate) {
-        this.activeItem.deselect();
-        this.activeItem = newUpdate;
-        this.activeItem.select();
+    goRight() {
+        if(this.index + 1 === this.items.length){
+           return; 
+        }
+        const index = this.index;
+        this.items[index].classList.remove('Carousel__item-focused');
+        this.items[index + 1].classList.add('Carousel__item-focused');
+        this.index++
     }    
-    
-    dataCar(data) {
-        return this.element.querySelector(`.Carousel__item[data-box="${data}"]`);
-    }
-    
 }
 
 

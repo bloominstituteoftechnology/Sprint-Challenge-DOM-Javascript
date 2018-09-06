a8465613422397a55ed2bb416b16c92e687e6fd8
sophiemullerc <32464200+sophiemullerc@users.noreply.github.com>
2018-09-05 23:56:16 -0700

    final commit

---
+++ ANSWERS.md
@@ -0,0 +1,14 @@
+1. What is the DOM?
+The Document Object Model (DOM) is a programming interface for HTML and XML documents. It represents the page so that programs can change the document structure, style, and content.
+
+2. What is an event?
+
+HTML DOM events allow JavaScript to register different event handlers on elements in an HTML document. Events are normally used in combination with functions, and the function will not be executed before the event occurs
+
+3. What is an event listener?
+
+Is a method that attaches an event handler to the specific element. It attaches an event handler to an element without overwriting existing event handlers.
+
+4. What is a Constructor?
+
+Is a subroutine that allow to create an object. Allows to the new object to be use, acepting arguments that the constructor uses for the variables.
--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,6 +1,48 @@
-class Carousel {
 
-}
+class CarouselItem {
+        constructor(element) {
+            this.element = element;
+        }
+        showItem(n) {
+            let slideIndex = 1;
+            let slide = this.element.classList.add(".Carousel__item-focused");
+            if (n > slide.length) slideIndex = 1;
+            if (n<1) slideIndex = slide.length;
+            for (let i = 0; i < slide.length; i++) {
+                slide[i].style.display = "none";
+            }
+            slide[slideIndex-1].style.display = "block"
+        }
+    }
+     class CarouselArrow {
+        constructor(element) {
+            this.element = element;
+            this.items = element.querySelectorAll(".Carousel__item");
+            this.items = Array.from(this.items).reduce((obj, item) => {
+                obj[item.dataset.carousel] = new CarouselItem(item);
+                return obj;
+            }, {});
+        }
+        rotateRight(slide) {
+           let slideIndex =1;
+           this.items.showItem(slideIndex += slide);
+        }
+        currentSlide(slide) {
+            let slideIndex;
+            this.items.showItem(slideIndex = n);
+        }
+    }
+    class Carousel {
+        constructor(element) {
+            this.element = element;
+            this.slideR = element.querySelectorAll(".Carousel__arrow-left");
+            this.slideL = element.querySelctorAll(".Carousel__arrow-right");
+    }
+    
+
+    }
+
 
 let carousels = document.querySelectorAll(".Carousel");
 carousels = Array.from(carousels).map(carousel => new Carousel(carousel));
+
